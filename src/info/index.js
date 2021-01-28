5ff4c11ddb1490c406605d8a8fd055ec8565a956
lalab996 <lalab996@hotmail.com.tw>
2021-01-28 11:45:54 +0800

    fix: info

--- src/info/index.js
+++ src/info/index.js
@@ -1,22 +1,20 @@
 export default {
-  address: "XXX",
-  googleSrc: "",
-  googleLink: "",
-  phone: "02-12345678",
-  fbLink: "",
+  address: "243新北市泰山區信華六街信華三街",
+  googleSrc:
+    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1027.2233086362949!2d121.439924!3d25.0443734!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7ddd60fbe9f%3A0xb575f9fad8e0edd4!2z5piO5pel5Z-OMuaOpeW-heS4reW_gw!5e1!3m2!1szh-TW!2stw!4v1611805321972!5m2!1szh-TW!2stw",
+  googleLink: "https://goo.gl/maps/wzA8yWbP9v4FE4J29",
+  phone: "02-2297-8899",
+  fbLink:
+    "https://www.facebook.com/%E6%98%8E%E6%97%A5%E5%9F%8E2-102181241308191/",
   fbMessage: "",
   caseName: "明日城2",
   indigatorLength: 10,
 
   houseInfos: [
-    ["投資新建", ""],
-    ["建築設計", ""],
-    ["美學顧問", ""],
-    ["結構工程", ""],
-    ["建築代銷", ""],
-    ["公設設計", ""],
-    ["建照號碼", ""],
-    ["建案位置", ""]
+    ["投資興建", "勝華建設有限公司"],
+    ["營造公司", ""],
+    ["企劃銷售", "新智力廣告有限公司"],
+    ["景觀設計", "上馥工程有限公司"]
   ],
 
   gtmCode: [""], // 可放置多個
