35c8f5f4f951930da90ffabc3f92e407054c5c3d
lalab996 <lalab996@hotmail.com.tw>
2021-03-25 14:31:02 +0800

    s7

--- src/projects/sv2/Section7.vue
+++ src/projects/sv2/Section7.vue
@@ -5,6 +5,9 @@
         v-for="item of swiperList"
         :key="item.id"
         :style="{ backgroundImage: `url(${item.imgUrl})` }"
+        data-aos="fade-left"
+        data-aos-duration="1500"
+        data-aos-delay="400"
       >
         <p>{{ item.title }}</p>
       </swiper-slide>
@@ -14,105 +17,107 @@
     </swiper>
 
     <div class="title">
-      <svg
-        v-if="!isMobile"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 386 563.53"
-      >
-        <g id="圖層_2" data-name="圖層 2">
-          <g id="圖層_1-2" data-name="圖層 1">
-            <text class="cls-1" transform="translate(21.45 32.72)">
-              和孩子一起 盡情快樂
-            </text>
-            <text class="cls-1" transform="translate(44.29 233.22)">
-              台灣蔦屋合作選冊
-            </text>
-            <text class="cls-2">
-              <tspan x="0" y="274.27">日系品牌設計美</tspan>
-              <tspan class="cls-3" x="154" y="274.27">學，</tspan>
-              <tspan x="188" y="274.27">讓閱讀時刻更有質感</tspan>
-              <tspan x="50" y="315.27">以書籍為中心的美好生活提案</tspan>
-              <tspan x="26" y="356.27">打造連</tspan>
-              <tspan class="cls-4" x="92" y="356.27">接</tspan>
-              <tspan class="cls-5" x="104.38" y="356.27">「人</tspan>
-              <tspan class="cls-6" x="147.62" y="356.27">」與</tspan>
-              <tspan class="cls-5" x="172.38" y="356.27">「</tspan>
-              <tspan x="194" y="356.27">夢</tspan>
-              <tspan class="cls-7" x="216" y="356.27">想</tspan>
-              <tspan class="cls-6" x="237.62" y="356.27">」</tspan>
-              <tspan x="250" y="356.27">的幸福聚所</tspan>
-            </text>
-            <circle class="cls-8" cx="191" cy="110.19" r="55" />
-            <circle class="cls-9" cx="195" cy="108.19" r="55" />
-            <text class="cls-10" transform="translate(174.85 100.59)">
-              蔦屋
-              <tspan x="-11" y="30">悅覽室</tspan>
-            </text>
-            <circle class="cls-9" cx="128.5" cy="432.19" r="55" />
-            <circle class="cls-11" cx="132.5" cy="430.19" r="55" />
-            <text class="cls-12" transform="translate(112.35 422.59)">
-              悅樂
-              <tspan x="-11" y="30">遊戲室</tspan>
-            </text>
-            <circle class="cls-9" cx="269.5" cy="432.19" r="55" />
-            <circle class="cls-11" cx="273.5" cy="430.19" r="55" />
-            <text class="cls-12" transform="translate(253.35 422.59)">
-              悅音
-              <tspan x="-11" y="30">練琴房</tspan>
-            </text>
+      <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="200">
+        <svg
+          v-if="!isMobile"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 386 563.53"
+        >
+          <g id="圖層_2" data-name="圖層 2">
+            <g id="圖層_1-2" data-name="圖層 1">
+              <text class="cls-1" transform="translate(21.45 32.72)">
+                和孩子一起 盡情快樂
+              </text>
+              <text class="cls-1" transform="translate(44.29 233.22)">
+                台灣蔦屋合作選冊
+              </text>
+              <text class="cls-2">
+                <tspan x="0" y="274.27">日系品牌設計美</tspan>
+                <tspan class="cls-3" x="154" y="274.27">學，</tspan>
+                <tspan x="188" y="274.27">讓閱讀時刻更有質感</tspan>
+                <tspan x="50" y="315.27">以書籍為中心的美好生活提案</tspan>
+                <tspan x="26" y="356.27">打造連</tspan>
+                <tspan class="cls-4" x="92" y="356.27">接</tspan>
+                <tspan class="cls-5" x="104.38" y="356.27">「人</tspan>
+                <tspan class="cls-6" x="147.62" y="356.27">」與</tspan>
+                <tspan class="cls-5" x="172.38" y="356.27">「</tspan>
+                <tspan x="194" y="356.27">夢</tspan>
+                <tspan class="cls-7" x="216" y="356.27">想</tspan>
+                <tspan class="cls-6" x="237.62" y="356.27">」</tspan>
+                <tspan x="250" y="356.27">的幸福聚所</tspan>
+              </text>
+              <circle class="cls-8" cx="191" cy="110.19" r="55" />
+              <circle class="cls-9" cx="195" cy="108.19" r="55" />
+              <text class="cls-10" transform="translate(174.85 100.59)">
+                蔦屋
+                <tspan x="-11" y="30">悅覽室</tspan>
+              </text>
+              <circle class="cls-9" cx="128.5" cy="432.19" r="55" />
+              <circle class="cls-11" cx="132.5" cy="430.19" r="55" />
+              <text class="cls-12" transform="translate(112.35 422.59)">
+                悅樂
+                <tspan x="-11" y="30">遊戲室</tspan>
+              </text>
+              <circle class="cls-9" cx="269.5" cy="432.19" r="55" />
+              <circle class="cls-11" cx="273.5" cy="430.19" r="55" />
+              <text class="cls-12" transform="translate(253.35 422.59)">
+                悅音
+                <tspan x="-11" y="30">練琴房</tspan>
+              </text>
+            </g>
           </g>
-        </g>
-      </svg>
+        </svg>
 
-      <svg
-        v-if="isMobile"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 326.52 557.53"
-      >
-        <g id="圖層_2" data-name="圖層 2">
-          <g id="圖層_1-2" data-name="圖層 1">
-            <text class="cls-1" transform="translate(26.06 26.4)">
-              和孩子一起 盡情快樂
-            </text>
-            <text class="cls-1" transform="translate(43.26 232.9)">
-              台灣蔦屋合作選冊
-            </text>
-            <text class="cls-2" transform="translate(8.1 277.27)">
-              日系品牌設計美
-              <tspan class="cls-3" x="123.2" y="0">學，</tspan>
-              <tspan x="150.4" y="0">讓閱讀時刻更有質感</tspan>
-              <tspan x="40" y="30">以書籍為中心的美好生活提案</tspan>
-              <tspan x="20.8" y="60">打造連</tspan>
-              <tspan class="cls-4" x="73.6" y="60">接</tspan>
-              <tspan class="cls-5" x="83.5" y="60">「人</tspan>
-              <tspan class="cls-4" x="118.1" y="60">」與</tspan>
-              <tspan class="cls-5" x="137.9" y="60">「</tspan>
-              <tspan x="155.2" y="60">夢</tspan>
-              <tspan class="cls-5" x="172.8" y="60">想</tspan>
-              <tspan class="cls-4" x="190.1" y="60">」</tspan>
-              <tspan class="cls-6" x="200" y="60">的幸福聚所</tspan>
-            </text>
-            <circle class="cls-7" cx="160.5" cy="117.19" r="55" />
-            <circle class="cls-8" cx="164.5" cy="115.19" r="55" />
-            <text class="cls-9" transform="translate(144.35 107.59)">
-              蔦屋
-              <tspan x="-11" y="30">悅覽室</tspan>
-            </text>
-            <circle class="cls-8" cx="90" cy="426.19" r="55" />
-            <circle class="cls-10" cx="94" cy="424.19" r="55" />
-            <text class="cls-11" transform="translate(73.85 416.59)">
-              悅樂
-              <tspan x="-11" y="30">遊戲室</tspan>
-            </text>
-            <circle class="cls-8" cx="231" cy="426.19" r="55" />
-            <circle class="cls-10" cx="235" cy="424.19" r="55" />
-            <text class="cls-11" transform="translate(214.85 416.59)">
-              悅音
-              <tspan x="-11" y="30">練琴房</tspan>
-            </text>
+        <svg
+          v-if="isMobile"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 326.52 557.53"
+        >
+          <g id="圖層_2" data-name="圖層 2">
+            <g id="圖層_1-2" data-name="圖層 1">
+              <text class="cls-1" transform="translate(26.06 26.4)">
+                和孩子一起 盡情快樂
+              </text>
+              <text class="cls-1" transform="translate(43.26 232.9)">
+                台灣蔦屋合作選冊
+              </text>
+              <text class="cls-2" transform="translate(8.1 277.27)">
+                日系品牌設計美
+                <tspan class="cls-3" x="123.2" y="0">學，</tspan>
+                <tspan x="150.4" y="0">讓閱讀時刻更有質感</tspan>
+                <tspan x="40" y="30">以書籍為中心的美好生活提案</tspan>
+                <tspan x="20.8" y="60">打造連</tspan>
+                <tspan class="cls-4" x="73.6" y="60">接</tspan>
+                <tspan class="cls-5" x="83.5" y="60">「人</tspan>
+                <tspan class="cls-4" x="118.1" y="60">」與</tspan>
+                <tspan class="cls-5" x="137.9" y="60">「</tspan>
+                <tspan x="155.2" y="60">夢</tspan>
+                <tspan class="cls-5" x="172.8" y="60">想</tspan>
+                <tspan class="cls-4" x="190.1" y="60">」</tspan>
+                <tspan class="cls-6" x="200" y="60">的幸福聚所</tspan>
+              </text>
+              <circle class="cls-7" cx="160.5" cy="117.19" r="55" />
+              <circle class="cls-8" cx="164.5" cy="115.19" r="55" />
+              <text class="cls-9" transform="translate(144.35 107.59)">
+                蔦屋
+                <tspan x="-11" y="30">悅覽室</tspan>
+              </text>
+              <circle class="cls-8" cx="90" cy="426.19" r="55" />
+              <circle class="cls-10" cx="94" cy="424.19" r="55" />
+              <text class="cls-11" transform="translate(73.85 416.59)">
+                悅樂
+                <tspan x="-11" y="30">遊戲室</tspan>
+              </text>
+              <circle class="cls-8" cx="231" cy="426.19" r="55" />
+              <circle class="cls-10" cx="235" cy="424.19" r="55" />
+              <text class="cls-11" transform="translate(214.85 416.59)">
+                悅音
+                <tspan x="-11" y="30">練琴房</tspan>
+              </text>
+            </g>
           </g>
-        </g>
-      </svg>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -120,7 +125,6 @@
 @import "~@/assets/style/variableDefault.scss";
 /* 螢幕尺寸標準 */
 .section7 {
-  display: none;
   width: 100%;
   height: 70vw;
   background-color: rgba(96, 96, 96, 0.2);
@@ -138,8 +142,8 @@
     svg {
       position: relative;
       width: 60%;
-      top: 50%;
-      transform: translateY(-45%);
+      //top: 50%;
+      transform: translateY(10%);
 
       .cls-1 {
         font-size: 37.18px;
@@ -262,7 +266,7 @@
         position: relative;
         width: 90%;
         top: 50%;
-        transform: translateY(-45%);
+        transform: translateY(5%);
 
         .cls-1 {
           font-size: 30px;
