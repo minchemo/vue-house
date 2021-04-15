5f015564f2fefac37721a114b9aab30c946a8c69
molly <molly8358158@gmail.com>
2021-04-15 13:58:24 +0800

    改GTM

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
     ["接待中心", "桃園市龜山區文桃路與樂善一路口"]
   ],
 
-  gtmCode: [""], // 可放置多個
+  gtmCode: ["P89J73T"], // 可放置多個
   recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
   recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
   recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
