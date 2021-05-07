96d20502ff76556d2342ce558f74e893c585ca82
molly <molly8358158@gmail.com>
2021-05-07 11:27:47 +0800

    新增GTM

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ["建照號碼", "(110)南工造字第01522號"]
   ],
 
-  gtmCode: [""], // 可放置多個
+  gtmCode: ["PB7F29N"], // 可放置多個
   recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
   recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
   recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
