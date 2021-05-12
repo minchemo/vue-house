92c9c7d663c807a4aa1c7f6643a9bf71492bd12d
minchemo <minchemo@minchemodeMacBook-Air.local>
2021-05-12 15:48:36 +0800

    head, contact-form

--- contact-form.php
+++ contact-form.php
@@ -1,4 +1,5 @@
 <?php
+    $src =$_SERVER['SERVER_NAME']; 
     $case_name = "西門大院";
     $case_code = 'sm';
 
@@ -238,7 +239,7 @@
     $mail->FromName = $case_name." - 官網網站"; //設定寄件者姓名
 
     $mail->Subject = $case_name." - 官網網站"; //設定郵件標題
-    $mail->Body = "網站：https://".$case_code.".h35.tw/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR>可聯絡時間：".$time_start."-".$time_end."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
+    $mail->Body = "網站：https://" . $src . "/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR>可聯絡時間：".$time_start."-".$time_end."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
     $mail->IsHTML(true); //設定郵件內容為HTML
 
     $tomail_arr = explode(",",$tomail);
