a8d56f7b79d2f32b8b2899d0e6b9b6ea83317aa5
molly <molly8358158@gmail.com>
2021-06-01 18:24:19 +0800

    修改選單位至

--- src/info/navList.js
+++ src/info/navList.js
@@ -1,7 +1,7 @@
 export default [
-  { name: '年年。好區', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '', offset: -50 },
-  { name: '年年。交通', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset: -50, mobileOffset: -50 },
-  { name: '年年。好學', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset:  720, mobileOffset: -50 },
+  { name: '年年。好區', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '', offset: -50, mobileOffset: -60 },
+  { name: '年年。交通', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset: -50, mobileOffset: -100 },
+  { name: '年年。好學', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset:  720, mobileOffset: 400 },
   { name: '年年。地標', imgSrc: '', subTitle: '', section: 'section4', svgSrc: '', offset: -50, mobileOffset: -50 },
   { name: '年年。團隊', imgSrc: '', subTitle: '', section: 'section5', svgSrc: '', offset: 0, mobileOffset: -50 },
   //{ name: '年年。公設', imgSrc: '', subTitle: '', section: 'section6', svgSrc: '', offset: -50, mobileOffset: -50 },
