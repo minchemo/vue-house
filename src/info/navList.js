fb58604c878e0b8bfb216e38f2c84e82906fa537
unknown <lalab996@hotmail.com.tw>
2021-06-04 18:02:50 +0800

    cby2

--- src/info/navList.js
+++ src/info/navList.js
@@ -1,5 +1,5 @@
 export default [
-  { name: '年年。好區', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '', offset: -50 },
+  { name: '年年。好區', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '', offset: -50, mobileOffset: 0  },
   { name: '年年。交通', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset: -50, mobileOffset: -100 },
   { name: '年年。好學', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset:  720, mobileOffset: 300 },
   { name: '年年。地標', imgSrc: '', subTitle: '', section: 'section4', svgSrc: '', offset: -50, mobileOffset: -50 },
