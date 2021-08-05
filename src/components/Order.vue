4257454c704ddf8235d66bb89c256d0b2170fde0
unknown <lalab996@hotmail.com.tw>
2021-08-05 12:22:05 +0800

    手機長度驗證

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -229,7 +229,7 @@ export default {
         this.isSubmit = false;
         return;
       }
-      if (this.form.phone.length < 10) {
+      if (this.form.phone.length != 10) {
         this.alertPhoneValidate();
         this.isSubmit = false;
         return;
