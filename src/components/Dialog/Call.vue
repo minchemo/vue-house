caeec67ef96a50f556782fd2baaa0d9ae2c50bb8
xcatx <xcatx796@gmail.com>
2021-12-08 17:09:00 +0800

    gp

--- src/components/Dialog/Call.vue
+++ src/components/Dialog/Call.vue
@@ -3,11 +3,12 @@
     <font-awesome-icon icon="phone" />
     <div class="dialog-desc">賞屋專線</div>
     <div class="info">{{ phone }}</div>
-    <div class="cta bt_contact" @click="redirectToPhoneThanks">撥打電話</div>
+    <div class="cta bt_contact" v-if="isMobile" @click="redirectToPhoneThanks">撥打電話</div>
   </div>
 </template>
 
 <script>
+import { isMobile } from '@/utils'
 export default {
   name: "CallDialog",
   components: {},
