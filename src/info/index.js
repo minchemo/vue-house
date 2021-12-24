9a252771040eed83ece3089608cfad2da42d6491
molly <molly8358158@gmail.com>
2021-12-24 11:30:28 +0800

    修改電話

--- src/info/index.js
+++ src/info/index.js
@@ -2,7 +2,7 @@ export default {
     address: "",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3614.3847262875265!2d121.4549993!3d25.0549462!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x43f7405f5c2b6594!2zMjXCsDAzJzE3LjgiTiAxMjHCsDI3JzI1LjkiRQ!5e0!3m2!1szh-TW!2stw!4v1638254348625!5m2!1szh-TW!2stw",
     googleLink: "https://goo.gl/maps/THRXYmqYuF4Ava7a6",
-    phone: "02-2276-7272",
+    phone: "06-299-2888",
     fbLink: "https://www.facebook.com/",
     fbMessage: "https://m.me//",
     caseName: "藏美表參道",
@@ -16,7 +16,7 @@ export default {
         ["基地面積", "487坪"]
     ],
 
-    gtmCode: ["MQCH8XK"], // 可放置多個
+    gtmCode: [""], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
