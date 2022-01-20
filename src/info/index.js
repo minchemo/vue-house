b4971e644e32d93344fc6c6dd0ca6fbb831589a4
molly <molly8358158@gmail.com>
2022-01-20 13:54:13 +0800

    改建案資訊

--- src/info/index.js
+++ src/info/index.js
@@ -1,7 +1,7 @@
 export default {
-    address: "台北市士林區天母西路120巷",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.503282281511!2d121.52056140055835!3d25.118670383853917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ae89aa6366b9%3A0x17a6a6b3f21d5d3e!2zMTEy5Y-w5YyX5biC5YyX5oqV5Y2A5aSp5q-N6KW_6LevMTIw5be3!5e0!3m2!1szh-TW!2stw!4v1642396353332!5m2!1szh-TW!2stw",
-    googleLink: "https://goo.gl/maps/kkhKxXJnzCjMAq5j8",
+    address: "台北市⼠林區天⺟東路109號",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.51337522257!2d121.53443251561319!3d25.11832894105839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ae7ffa20dac9%3A0x7d52e55d904bedfc!2zMTEx5Y-w5YyX5biC5aOr5p6X5Y2A5aSp5q-N5p2x6LevMTA56Jmf!5e0!3m2!1szh-TW!2stw!4v1642581220742!5m2!1szh-TW!2stw",
+    googleLink: "https://goo.gl/maps/rUD7Six2r6v8Adqc9",
     phone: "02-88096688",
     fbLink: "https://www.facebook.com/",
     fbMessage: "https://m.me//",
@@ -9,11 +9,12 @@ export default {
     indigatorLength: 10,
     houseInfos: [
         ["投資興建", "力麒建設股份有限公司"],
-        ["建築規劃", "李天鐸建築師"],
-        ["結構工程", "築遠工程顧問"],
+        ["建築規劃", "李天鐸建築師事務所"],
+        ["結構工程", "築遠工程顧問有限公司"],
         ["建築代銷", "海悅開發股份有限公司"],
         ["貴賓專線", "(02)2871-3668"],
-        ["基地位置", "台北市士林區天母西路120巷旁"],
+        ["基地位置", "台北市北投區天母西路120巷旁"],
+        ["接待中⼼", "台北市⼠林區天⺟東路109號"],
     ],
 
     gtmCode: [""], // 可放置多個
