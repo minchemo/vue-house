1e5ed058738054d96fab1ad54e087d272cf23710
xcatx <xcatx796@gmail.com>
2022-02-08 16:40:36 +0800

    sv2

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Noto+Serif+TC:200,300,400,500,600,700,900&subset=chinese-traditional');
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400,500,700,900&subset=chinese-traditional');
+//@import url('https://fonts.googleapis.com/css?family=Noto+Serif+TC:200,300,400,500,600,700,900&subset=chinese-traditional');
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC:300,400,500,700,900&subset=chinese-traditional');
 // @import url('https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap');
 </style>
 
