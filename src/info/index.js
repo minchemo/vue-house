87a25a652a013537a85338f21e54d8b92c020bc3
molly <molly8358158@gmail.com>
2022-04-14 15:14:11 +0800

    增加GTM

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
         ["不動產經紀人", "徐福龍(91)北市經證字第00848號"],
     ],
 
-    gtmCode: ["WFTQQ9G"], // 可放置多個
+    gtmCode: ["WFTQQ9G","MMKBJ8C"], // 可放置多個
     recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC",
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
