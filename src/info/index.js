631282c4e487d4882f2ae39916bcd6734286fff5
molly <molly8358158@gmail.com>
2022-04-14 15:08:22 +0800

    增加GTM

--- src/info/index.js
+++ src/info/index.js
@@ -22,7 +22,7 @@ export default {
         ["不動產經紀人", "林建汶"]
     ],
 
-    gtmCode: ["KLM3QRV"], // 可放置多個
+    gtmCode: ["KLM3QRV","KLRVLF2"], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
