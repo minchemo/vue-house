8bb8ea57e7bc80d90414c2a0dff07d1036dfefb7
unknown <lalab996@hotmail.com.tw>
2022-04-26 21:36:33 +0800

    orderbg

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -16,6 +16,23 @@
         <img v-else src="~@/projects/cc/logo.png" alt="青青" srcset="" />
       </div>
       <div class="order-title" v-html="order.title"></div>
+
+      <img
+        v-if="!isMobile"
+        class="bg-img"
+        src="@/projects/cc/contact.png"
+        alt=""
+        srcset=""
+        uk-parallax="viewport:0.8;y:200,0;"
+      />
+      <img
+        v-else
+        class="bg-img"
+        src="@/projects/cc/contact-mo.png"
+        alt=""
+        srcset=""
+        uk-parallax="viewport:0.8;y:0;"
+      />
       <div class="order-subtitle" v-html="order.subTitle"></div>
       <div class="order">
         <div class="form">
