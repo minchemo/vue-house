55df6ffbc50dfdec911d28a510a3cd79629e9fea
unknown <lalab996@hotmail.com.tw>
2022-06-16 02:01:05 +0800

    updat

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,8 +4,8 @@
     'opacity-0': !isLoading,
     'pointer-events-none': !isLoading
   }"
-    class="transition-all duration-500	 flex items-center justify-center fixed w-screen h-screen top-0 left-0 bg-white z-[100]">
-    <img class="w-48" src="@/assets/loading_w.gif" alt="" srcset="">
+    class="transition-all duration-500	flex-col flex items-center justify-center fixed w-screen h-screen top-0 left-0 bg-white z-[100]">
+    <img class="w-32" src="@/assets/loading_w.gif" alt="" srcset="">
   </div>
   <!--loading end-->
   <Nav />
@@ -23,6 +23,7 @@
 </template>
 
 <script setup>
+import info from "@/info"
 import S1 from "@/section/s1.vue"
 import S2 from "@/section/s2.vue"
 import S3 from "@/section/s3.vue"
