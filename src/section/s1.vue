b162aa477d4b4d6125ca5f0bc32c81d96dd87c21
xcatx <xcatx796@gmail.com>
2022-09-19 10:54:34 +0800

    1

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -1,35 +1,25 @@
 <template>
-  <article class="s1 font-['noto_serif_tc','serif']">
+  <article class="s1 font-['Noto_sans_tc','serif']">
     <div class="img">
-      <img src="./s1/img.jpg" class="imgv" alt="img" v-if="!isMobile">
-      <img src="./s1/img_m.jpg" class="imgv" alt="img" v-else>
-
-      <Rabbit class="rabbit" />
-      <div class="txt" data-aos="zoom-in-right">上學只要<span><b>1</b>分鐘</span></div>
+      <img src="./s1/00.png" class="cat" alt="cat" data-aos="zoom-in">
+      <img src="./s1/t4.svg" class="imgt" alt="t4" data-aos="zoom-in-right">
       <!--div class="txt2" v-if="isMobile">日進月步にっしんげっぽ</div-->
     </div>
-    <img src="./s1/leaf6.png" v-if="!isMobile" class="leaf6" alt="leaf">
-
-    <div class="txtall">
-      <img src="./s1/leaf1.png" v-if="!isMobile" data-aos="fade" data-aos-delay="300" class="leaf1" alt="leaf">
-      <img src="./s1/leaf2.png" v-if="!isMobile" data-aos="fade" data-aos-delay="100" class="leaf2" alt="leaf">
-      <img src="./s1/leaf3.png" v-if="!isMobile" data-aos="fade" class="leaf3" alt="leaf">
-      <img src="./s1/leaf4.png" v-if="!isMobile" data-aos="fade" data-aos-delay="200" class="leaf4" alt="leaf">
-      <img src="./s1/leaf5.png" v-if="!isMobile" data-aos="fade" data-aos-delay="400" class="leaf5" alt="leaf">
-      <img src="./s1/book1.png" v-if="!isMobile" data-aos="fade-down" class="book1" alt="book">
-      <img src="./s1/book2.png" v-if="!isMobile" data-aos="fade-down" data-aos-delay="300" class="book2" alt="book">
-
-      <img src="./s1/logo.svg" class="logo" alt="logo" v-if="!isMobile" data-aos="zoom-in">
-      <img src="./s1/logom.svg" class="logo" alt="logo" v-else data-aos="zoom-in">
-      <h3 class="txt" v-if="!isMobile">
-        <div class="t1" data-aos="zoom-in" data-aos-delay="200"><span class="">成州國小正對面</span><span>戶戶邊間正3房</span></div>
-        <div class="t2" data-aos="zoom-in" data-aos-delay="400">23-29坪<span>｜</span>02-8295-3311</div>
-      </h3>
-      <h3 class="txt" v-else>
-        <div class="t1" data-aos="zoom-in" data-aos-delay="200"><span class="">成州國小正對面</span><span>戶戶邊間正3房</span></div>
-        <div class="t2" data-aos="zoom-in" data-aos-delay="400">23-29坪<span>｜</span>02-8295-3311</div>
-      </h3>
-    </div>
+      <div class="txt">
+        <div class="t1" data-aos="zoom-in" data-aos-delay="0">
+          <img src="./s1/t1.svg" class="" alt="logo">
+          <img src="./s1/t2.svg" class="" alt="logo">
+        </div>
+        <div class="t2" data-aos="zoom-in" data-aos-delay="100">
+          <img src="./s1/t3.svg" class="" alt="logo" v-if="!isMobile">
+          <img src="./s1/t3m.svg" class="" alt="logo" v-else>
+        </div>
+        <div class="t3" data-aos="zoom-in" data-aos-delay="200">
+          <img src="./s1/logo.svg" class="" alt="logo">
+          <img src="./s1/t5.svg" class="" alt="logo">
+        </div>
+      </div>
+    <img src="./s1/slogo.svg" alt="slogo" class="slogo absolute" />
   </article>
 </template>
 
@@ -37,7 +27,7 @@
 @import '@/assets/style/function.scss';
 
 .s1 {
-  // background: url('./s1/pc.jpg') center center no-repeat;
+  background: url('./s1/bg.jpg') center bottom no-repeat;
   background-size: contain;
   width: 100%;
   height: 100vh;
@@ -48,181 +38,63 @@
   background-color: #FFEF00;
 }
 
+.slogo{
+  left: 0;
+  bottom: 0;
+  width: size(150);
+}
 .img {
   position: absolute;
   top: 50%;
   left: 0;
 
-  .imgv {
-    width: size(770);
-    position: absolute;
-    top: size(40);
-    left: 0;
-  }
 
-  .rabbit {
+  .cat {
     position: absolute;
-    top: size(-420);
-    left: size(210);
-    width: size(455);
+    top: size(-360);
+    left: size(113);
+    width: size(430);
   }
+  .imgt{    
+    position: absolute;
+    top: size(-340);
+    left: size(465);
+    width: size(215);
+  }
+}
+
 
   .txt {
-    position: absolute;
-    width: 6.57em;
-    height: 6.57em;
-    color: #fff;
-    border-radius: 50%;
-    background: #00B451;
-    left: 20.17em;
-    top: -5.16em;
-    font-size: size(30);
-    padding: 1.6em 0 0 0;
-    line-height: 1.3;
-    transform-origin: 0 100%;
-    text-align: center;
-
-    &::before {
-      content: "";
-      width: 1.9em;
-      height: 1.9em;
-      ;
-      display: block;
-      background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 53 53'%3E%3Cpath fill='%2300B451' d='M32.2,0.3l20.8,27L6.2,52c-2.3,1.2-4.7-1.4-3.4-3.6L32.2,0.3z'/%3E%3C/svg%3E") center no-repeat;
-      background-size: contain;
-      position: absolute;
-      bottom: 0;
-      left: -0.5em;
-    }
-
-    span {
-      display: block;
-      font-size: 1.5em;
-
-      b {
-        font-weight: 600;
-        font-size: 1.15em;
-        display: inline-block;
-        vertical-align: -0.07em;
-      }
-    }
-  }
-}
-
-.leaf1 {
-  position: absolute;
-  top: size(130);
-  right: size(200);
-  width: size(70);
-  transform: translate(-150%, -150%);
-}
-
-.leaf2 {
-  position: absolute;
-  top: size(10);
-  right: size(230);
-  width: size(58);
-  transform: translate(150%, -150%);
-}
-
-.leaf3 {
-  position: absolute;
-  top: size(230);
-  right: size(330);
-  width: size(100);
-  transform: translate(150%, -150%);
-}
-
-.leaf4 {
-  position: absolute;
-  top: size(330);
-  right: size(265);
-  width: size(85);
-  transform: translate(150%, -150%);
-}
-
-.leaf5 {
-  position: absolute;
-  top: size(490);
-  left: size(120);
-  width: size(85);
-  transform: translate(-150%, -150%);
-}
-
-.book1 {
-  position: absolute;
-  top: size(280);
-  left: size(170);
-  width: size(190);
-}
-
-.book2 {
-  position: absolute;
-  top: size(80);
-  left: size(235);
-  width: size(180);
-}
-
-.leaf6 {
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: size(439);
-}
-
-@keyframes ro {
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-.txtall {
-  position: absolute;
-  top: calc(50% + (175 - 540) * 100vw / 1920);
-  right: 0;
-  width: size(1200);
-  text-align: center;
-
-  .logo {
-    width: size(271);
-  }
-
-  .txt {
-    //@include img_c_pc(1200, 140);
-    //@extendtop:size(58); //calc(50% + (150 - 540) * 100vw / 1920);
+    width: size(1240);
+    height: 100%;
 
     line-height: 1.2;
     letter-spacing: 0em;
     text-align: center;
     z-index: 3;
-    color: #000;
-    margin: 1.4em 0 0 0;
+    color: #E52C29;
+    margin: 0 0 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction:column;
+    flex-wrap: wrap;
+    font-size: size(80);
+    padding: 0 0 2em;
 
   }
 
   .t1 {
-    display: flex;
-    font-weight: 700;
-    font-size: size(50);
-    align-items: center;
-    justify-content: center;
-    gap: size(12);
-
-    span:nth-child(1) {
-      color: #46B258;
-    }
+    img{height: size(157); margin: 0 0.05em 0 0.15em;}
   }
 
   .t2 {
-    display: inline-block;
-    font-weight: 600;
-    font-size: size(48);
-
-    span {
-      color: #46B258;
-    }
+    img{height: size(114);  margin: 0.63em 0 0.65em;}
   }
-}
+  .t3 {
+    img{height: size(120); margin: 0 0.3em 0 0.3em;}
+  }
+
 
 /* 螢幕尺寸標準 */
 /* 平板尺寸 */
@@ -233,84 +105,66 @@
   // .a1 {width: 100%;position: absolute;left: 0;top:size-m(-63);opacity: 0.50;}
   .s1 {
     min-height: size-m(667);
-    max-height: size-m(750);
+    max-height: size-m(950);
+    height: size-m(750);
     overflow: hidden;
-    background: url('./s1/mobg.jpg') center;
+    background-image: url('./s1/bgm.jpg');
     background-size: contain;
   }
+.slogo{
+  left: 0;
+  bottom: 0;
+  width: size-m(100);
+}
 
   .img {
     width: 100%;
     top: auto;
     bottom: 0;
-
-    .imgv {
-      width: 100%;
-      top: auto;
-      bottom: 0;
-      // opacity: 0;
-    }
-
-    .rabbit {
-      top: size-m(-399);
-      left: size-m(69);
-      width: size-m(249);
-    }
-
-    .txt {
-      left: size-m(270);
-      top: size-m(-250);
-      font-size: size-m(13);
-    }
-
-    .txt2 {
-      position: absolute;
-      left: 0;
-      width: 100%;
-      bottom: size-m(10);
-      font-size: size-m(32);
-      color: #fff;
-      text-shadow: 0.05em 0.05em 0.2em #000;
-    }
+  .cat {
+    position: absolute;
+    top: size-m(-258);
+    left: size-m(115);
+    width: size-m(167);
+  }
+  .imgt{    
+    position: absolute;
+    top: size-m(-245);
+    left: size-m(250);
+    width: size-m(85);
   }
 
-  .txtall {
-    top: calc(50% + (40 - 333.5) * 100vw / 375);
-    width: 100%;
 
-    .logo {
-      width: size-m(230);
-    }
-
+  }
     .txt {
+      width: 100%;
       font-size: size-m(26.2);
       font-weight: 600;
       margin: .7em 0 0 0;
       line-height: 1.3;
+    padding: 0 0 9.6em;
     }
 
     .t1 {
-      font-size: size-m(30);
-      flex-direction: column;
-      gap: size-m(2);
-
-      span:nth-child(1) {
-        color: #46B258;
-      }
+    img{height: size-m(62); margin: 0;
+    &:first-child{
+      height: size-m(80);margin: 0 0 1.2em 0;
+    }
+    
+    }
     }
 
     .t2 {
-      display: block;
-      font-weight: 600;
-      font-size: size-m(20);
-      margin-top: size-m(12);
-
-      span {
-        color: #46B258;
-      }
+    img{height: size-m(114); margin:.9em 0 0.65em; }
     }
-
+  .t3 {
+    img{height: size-m(40); margin: 0;
+    &:first-child{
+      height: size-m(50);margin: 0 0 0.7em 0;
+    }}
   }
+
+
 
 }
 </style>
