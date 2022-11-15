2815d23f60093358cd394d2311c8d6ffccf6ac3d
molly <molly8358158@gmail.com>
2022-11-15 09:11:03 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -16,7 +16,7 @@ export default {
         ["基地面績", "487坪"]
     ],
 
-    gtmCode: ["MQCH8XK"], // 可放置多個
+    gtmCode: ["WJ45C34"], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
