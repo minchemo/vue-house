6b47b0d0eac24a888475f909868586cb0affed9e
xcatx <xcatx796@gmail.com>
2022-12-08 10:14:15 +0800

    02

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,8 +18,10 @@
     <S5 />
     <S6 />
     <S7 />
+    <!--
     <S8 id="section8" />
-   <!-- <S9 /> --> 
+    <S9 />
+    --> 
     <Order />
   </div>
 </template>
@@ -43,8 +45,10 @@ import S4 from "@/section/s4.vue"
 import S5 from "@/section/s5.vue"
 import S6 from "@/section/s6.vue"
 import S7 from "@/section/s7.vue"
+/*
 import S8 from "@/section/s8.vue"
-// import S9 from "@/section/s9.vue"
+import S9 from "@/section/s9.vue"
+*/
 import Order from "@/section/order.vue"
 import Nav from "@/layout/navbar.vue"
 import { onMounted, ref } from "vue"
