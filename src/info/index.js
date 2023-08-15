de06328f4593aaf2d7061f4c36189e188fec46fe
molly <molly8358158@gmail.com>
2023-08-15 15:16:16 +0800

    修改地址

--- src/info/index.js
+++ src/info/index.js
@@ -1,8 +1,8 @@
 import { isMobile } from "../utils/index";
 export default {
-    address: '新北市五股區成泰路三段580號',
-    googleSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57806.504097274774!2d121.37842134863278!3d25.105026000000016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442af3ce52d7e1f%3A0xfc36e6280758b4a5!2z5Ly05pyI54Gj!5e0!3m2!1szh-TW!2stw!4v1688086827059!5m2!1szh-TW!2stw',
-    googleLink: 'https://goo.gl/maps/WFLug6HCPioVuGYKA',
+    address: '新北市五股區疏洪北路成州八路口',
+    googleSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.9072377450757!2d121.45206197640343!3d25.10500123542692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442af213b157e05%3A0x7725924cb26159c7!2z5Ly05pyI54Gj5o6l5b6F5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1692083681953!5m2!1szh-TW!2stw',
+    googleLink: 'https://goo.gl/maps/BHpkhhfrUc2HDGxy8',
     phone: "02-82922333",
     fbLink: "https://www.facebook.com/103573018971658/",
     fbMessage: "https://m.me/103573018971658/",
