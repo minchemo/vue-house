750ebee88302861173c73d4d89a1d3f12af65455
xcatx <xcatx796@gmail.com>
2023-08-23 16:54:53 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -25,7 +25,7 @@ export default {
         ["燈光設計", "OuDe light 偶得設計"],
         ["格局規劃", "全三房"],
     ],
-    gtmCode: ["GTM-PQ8LQ84S"], // 可放置多個
+    gtmCode: ["GTM-W2SDWJ63"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
