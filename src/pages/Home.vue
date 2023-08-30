722bf4c2ff74e4a32711129713cf74ee88006168
xcatx <xcatx796@gmail.com>
2023-08-30 11:05:14 +0800

    sigma1

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -23,12 +23,21 @@
     <div class="bg3">
     <div class="aurora">
       <div>
+      <img src="@/section/s1/gif2.gif" alt="" />
+      </div>
+    </div>
+    </div>
+
+      <!-- 
+    <div class="bg3">
+    <div class="aurora">
+      <div>
       <img src="@/section/s1/aurora.png" alt="" />
       <img src="@/section/s1/aurora.png" alt="" />
       </div>
     </div>
     </div>
-
+-->
 
    <!-- <div class="bg3">
     <div class="aurora">
@@ -73,7 +82,22 @@ img{width: 100%;position:absolute;left: 0;
   opacity: .8;mix-blend-mode: screen;
   img{position: relative;width:120%;filter: blur(1px);margin: -5px;max-width: 120%;}
 }
+.bg3{position:absolute;left: 0;top: 0;width:100%;height: 140vw;
+  overflow: hidden;mix-blend-mode: screen;
+   animation: hue 1.5s linear infinite alternate;filter: hue-rotate(50deg);
+.aurora{width:100%;
+  transform: rotate(-5deg)translate(-25%,-10%);
+  transform-origin: 0 0; 
+ div{width: 100%;
+  transform: perspective(1.3em)rotateX(.5deg);
+  transform-origin: 50% 50%;
+}
+img{width:150%;filter:blur(3px);max-width: 900%;}
+}
+}
+/*
 .bg3{position:absolute;left: 0;top: 0;width:100%;overflow: hidden;
+    height: 140vw;
    animation: hue 1.5s linear infinite alternate;filter: hue-rotate(50deg);
 .aurora{width:250vw;height:400vw;position:relative;overflow: hidden;
    transform: rotate(-16deg)translate(-35%,-40%);
@@ -81,10 +105,10 @@ img{width: 100%;position:absolute;left: 0;
 div{white-space: nowrap;width:100%;height: 100%;
  transform: perspective(50px)rotateX(3deg);
   transform-origin: 50% 50%;
-img{display: inline-block;width:100%;height: 70%;animation: aurora 20s linear infinite reverse;filter:blur(1vw);}
+img{display: inline-block;width:100%;height: 70%;animation: aurora 30s linear infinite reverse;filter:blur(1vw);}
 }
 }
-}
+}*/
 /*
 .bg3{position:absolute;left: 0;top: 0;width:100%;overflow: hidden;}
 .aurora{position:relative;left: 0;width:130vw;height:130vw;top:0;
@@ -116,13 +140,13 @@ img{display: inline-block;width:100%;height: 70%;animation: aurora 20s linear in
 &:nth-child(2){top: auto;left: 0;}
 &:last-child{bottom: auto;right: 0;}
 }
-}
+}/*
 .bg3{height:400vw;
 .aurora{width:420vw;height:400vw;
   transform: rotate(-100deg)translate(-100%, -35%);
   img{ filter:blur(3vw);}
 }
-}
+}*/
 /*
 */
 }
