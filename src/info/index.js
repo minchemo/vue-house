9a3c4ad73dcd35471c4e6f5b6dcd2a0c4b29d1c3
xcatx <xcatx796@gmail.com>
2023-09-12 13:51:08 +0800

    0

--- src/info/index.js
+++ src/info/index.js
@@ -1,4 +1,4 @@
-const caseName = "微笑歐洲"
+const caseName = "萬企君品"
 
 export default {
     meta: {
@@ -8,21 +8,25 @@ export default {
     },
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "台中市梧棲區建國北街31巷158號",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3637.800898233423!2d120.5306116!3d24.2487415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34691569a3de9af9%3A0x83cfbf71cb2093bb!2z6bqX55ubLeW-rueskeatkOa0sg!5e0!3m2!1szh-TW!2stw!4v1689321806178!5m2!1szh-TW!2stw",
-    googleLink: "https://goo.gl/maps/wkjqMvf9LVWwjuF99",
-    phone: "04-26562089",
-    fbLink: "https://www.facebook.com/lihshingsmileeurope",
-    fbMessage: "https://m.me/lihshingsmileeurope",
+    address: "新北市淡水區新民街65號",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902.6985236410554!2d121.4377642696526!3d25.176429512310555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a5c6ecb65785%3A0x1945836ec1535297!2z6JCs5LyB5ZCb5ZOB5o6l5b6F5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1689059072363!5m2!1szh-TW!2stw",
+    googleLink: "https://goo.gl/maps/xaf4sYQFGJNJ7LB26",
+    phone: "02-2626-9393",
+    fbLink: "https://www.facebook.com/100089552726620",
+    fbMessage: "https://m.me/100089552726620",
     caseName: caseName,
     houseInfos: [
-        ["投資興建", "麗寶集團-麗盛建設"],
-        ["建築設計", "陳明偉建築師 "],
-        ["使照號碼", "109中都使字第01065號等2個"],
-        ["行銷企劃", "海沃創意行銷"],
+        ["投資興建", "建泰工藝"],
+        ["建築設計", "三門聯合建築師事務所 "],
+        ["結構設計", "織本結構株式會社"],
+        ["工程營造", "順緯營造"],
+        ["企劃銷售", "梁茂廣告"],
+        ["建照號碼", "110建字第0357號"],
+        ["證照號碼", "(112)雲縣地字000229號"],
+        ["經紀人", "廖哲志"], 
     ],
-    gtmCode: "GTM-KZ6H36GQ", // 可放置多個
-    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
+    gtmCode: ["GTM-WJBN353"], // 可放置多個
+    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
@@ -32,47 +36,41 @@ export default {
         subTitle: "歡迎預約，將有專人與您聯絡，我們將竭誠為您服務",
     },
     navList: [{
-            name: "微笑歐洲",
-            target: ".s1",
-            offset: "",
-        },{
-            name: "10大看點",
+            name: "重大建設",
             target: ".s3",
             offset: "",
         },{
-            name: "大墅人生",
+            name: "公園特區",
             target: ".s4",
             offset: "",
         },{
-            name: "機能在手",
+            name: "生活機能",
+            target: ".s5",
+            offset: "",
+        },{
+            name: "制震地標",
             target: ".s6",
             offset: "",
         },{
-            name: "港灣錢潮",
+            name: "會館公設",
             target: ".s7",
             offset: "",
         },{
-            name: "麗寶承諾",
+            name: "加值服務",
+            target: ".s8",
+            offset: "",
+        },{
+            name: "選書合作",
             target: ".s9",
             offset: "",
         },{
-            name: "立即來電",
-            target: ".contact-info",
+            name: "誠意保固",
+            target: ".s10",
             offset: "",
-            type: 'btn'
-        },
-        {
-              name: "地圖導航",
-              target: ".gmap",
-              offset: "",
-              type: "btn",
-        }
-        ,{
-            name: "立即預約",
-            target: ".order",
+        },{
+            name: "國際建材",
+            target: ".s10",
             offset: "",
-            type: 'btn'
         },
     ],
-    
 }
--- src/section/s1.vue
+++ src/section/s1.vue
@@ -5,12 +5,15 @@
     <img src="./s1/t.svg" class="t12" alt="" data-aos="zoom-out-down" v-if="!$isMobile()"/>
     <img src="./s1/tm.svg" class="t12" alt="" data-aos="zoom-out-down" v-if="$isMobile()"/> -->
        <!--  <div class="slide" @click="scrollTo('#s2')" data-aos="zoom-out" data-aos-delay="800">SLIDE</div> -->
-    <img class="bg" src="./s1/bg.jpg" alt="" srcset="">
-    <img src="./s1/logo.svg" class="logo" alt="" data-aos="zoom-in" data-aos-delay="0" data-aos-duration="1000"/>
-       <div class="txt" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1000">
-        <span class="t1">實坪電梯別墅｜樂享花園會館</span><span class="t2">64-74坪｜大器落成｜正式公開</span>
-      </div>
-
+       <!--div class="txt font-['noto_serif_tc']" data-aos="zoom-out" data-aos-duration="1000"><span class="t1">高鐵<br  v-if="$isMobile()"/>台積宅</span><span class="t2">2</span><span class="t3">字頭</span><span class="t4">THE GREEN ROAD</span></div-->
+    <div class="love"><img src="./s1/love.png" alt="" data-aos="zoom-out" data-aos-delay="300" data-aos-duration="1000"/></div>
+    <div class="love1" v-if="!$isMobile()"><img src="./s1/love.png" alt=""></div>
+    <div class="love2" v-if="!$isMobile()"><img src="./s1/love.png" alt=""></div>
+    <img src="./s1/bg1.png" v-if="!$isMobile()" class="bg1" alt=""/>
+    <img src="./s1/bg2.png" v-if="!$isMobile()" class="bg2" alt=""/>
+    <img src="./s1/bg_m.png" v-if="$isMobile()" class="bg1" alt=""/>
+    <img src="./s1/logo.svg" class="logo" alt="" data-aos="zoom-out" data-aos-delay="300" data-aos-duration="1000"/>
+    <img src="./s1/t.svg" class="txt" alt="" data-aos="zoom-out" data-aos-delay="300" data-aos-duration="1000"/>
   </article>
 </template>
 
@@ -18,36 +21,80 @@
 @import '@/assets/style/function.scss';
 
 .s1 {
+  
   @apply relative w-full h-screen;
-  min-height: size(1000);
-  max-height: size(1180);
-  height: calc(100vh + 100vw * 100 /1920);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items:center;
-  font-size:size(25);
-
-   .t0{position: absolute;top: 0;left: 0;width: 100%;opacity: .5;z-index: 2;}
-   .bg{position: absolute;bottom:0;left: 0;width: 100%;}
-   .txt{
-    text-align: center;
-    line-height:1.5;
-   position: relative;
-   left: -.5em;
-    color: #FFF;
+  min-height: size(900);
+  max-height: size(1080);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items:center;
-    margin: 0 10em 4.8em 2em;font-size:1.28em;
-    .t1{font-size:1.1em;display: inline-block;transform: scaleX(.96);
-      font-weight: 700;margin: 0;
-    text-align: left;letter-spacing: .05em;  color: #171C60;}
-    .t2{font-size: 1em;position: relative;top: .03em;left: -.04em;margin: auto -0.01em;  background: linear-gradient(180deg, #E48726 0%, #E27E26 10.00%, #DE6929 41.00%, #DB5C2A 72.00%, #DB582B 100%);padding: 0.3em;
+  font-size:size(25);
+
+    img{width:100%;}
+    .love{
+    z-index: 1;
+    position: absolute;  
+    width: size(850);
+    left: size(105);
+    top: calc(50% + #{size(115 - 540)});
     }
+    .love1{
+    z-index: 1;
+    position: absolute;  
+    width: size(280);
+    right: size(620);
+    bottom: size(70); 
+    animation: an 3s ease-in-out infinite alternate-reverse;
+     transform:translate(10%, 20%);
+     img{transform: scaleX(-1)rotate(-7deg);}
+    }
+    .love2{
+    z-index: 1;
+    position: absolute;  
+    width: size(125);
+    right: size(285);
+    top: size(95); 
+    animation: an 3s ease-in-out infinite alternate-reverse;
+     transform:translate(20%, -20%);
+     img{transform: rotate(-7deg);}
+    }
+
+    @keyframes an {
+  to {
+     transform:translate(0px);
   }
-  .logo{width: size(503);margin: -2em 0 0 0;}
+}
+
+    .bg1{
+     z-index: 0;
+    position: absolute;
+    bottom:size(0);
+    }
+
+    .bg2{
+    z-index: 2;
+    position: absolute;
+    bottom:size(0);
+    }
+   .txt{
+    z-index: 2;
+    width: size(698);
+    position: absolute;
+    right: size(115);
+    top: calc(50% + #{size(285 - 540)});
+  }
+  .logo{
+    position: absolute;
+    width: size(627);
+    z-index: 2;
+    left: size(215);
+    top: calc(50% + #{size(385 - 540)});
+    }
+
+  
+
+    
 
 }
 
@@ -58,20 +105,40 @@
 
 @media screen and (max-width: 767px) {
 
+
+
   .s1 {
     height:calc(100vh - 63px);
-    min-height: sizem(660);
-    max-height: sizem(770);
-    font-size:sizem(21);
-  flex-direction: column;
-   .bg{left: -20%;width: 120%;max-width: 900%;}
+    min-height: sizem(766);
+    max-height: sizem(766);
+    font-size:sizem(12);
+
    .txt{
-    margin: 0 auto 7.5em auto;
-    font-size:1em;
-   left: 0em;
-   }
-  .logo{width: sizem(300);margin: 0em auto 1em auto;}
+    width: sizem(324);
+    position: absolute;
+    right: sizem(25);
+    bottom: sizem(185);
+    top:auto;
   }
+  .logo{
+    width: sizem(302);
+    left: sizem(36.2);
+    top: sizem(153);
+    }
+
+  .love{ 
+    width: sizem(350);
+    left: sizem(12);
+    top: sizem(59);
+    }
+
+  .bg1{
+      width: sizem(375);
+    }
+
+  }
+
+  
 }
 </style>
 <script setup>
