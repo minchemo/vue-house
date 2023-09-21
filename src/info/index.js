7a90a178f021630dd84fb209febed25cd63fa053
molly <molly8358158@gmail.com>
2023-09-21 14:31:27 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -1,4 +1,4 @@
-const caseName = "萬企君品"
+const caseName = "國鉅興"
 
 export default {
     meta: {
@@ -8,25 +8,21 @@ export default {
     },
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "新北市淡水區新民街65號",
+    address: "新北市泰山區莊田路62號旁",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902.6985236410554!2d121.4377642696526!3d25.176429512310555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a5c6ecb65785%3A0x1945836ec1535297!2z6JCs5LyB5ZCb5ZOB5o6l5b6F5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1689059072363!5m2!1szh-TW!2stw",
     googleLink: "https://goo.gl/maps/xaf4sYQFGJNJ7LB26",
     phone: "02-2626-9393",
-    fbLink: "https://www.facebook.com/100089552726620",
-    fbMessage: "https://m.me/100089552726620",
+    fbLink: "https://www.facebook.com/100094458446747",
+    fbMessage: "https://m.me/100094458446747",
     caseName: caseName,
     houseInfos: [
-        ["投資興建", "建泰工藝"],
-        ["建築設計", "三門聯合建築師事務所 "],
-        ["結構設計", "織本結構株式會社"],
-        ["工程營造", "順緯營造"],
-        ["企劃銷售", "梁茂廣告"],
-        ["建照號碼", "110建字第0357號"],
-        ["證照號碼", "(112)雲縣地字000229號"],
-        ["經紀人", "廖哲志"], 
+        ["投資興建", "國鉅開發建設股份有限公司"],
+        ["企劃銷售", "澄品廣告股份有限公司"],
+        ["房型規劃", "2-3房"],
+        ["坪數規劃", "17-32坪"],
     ],
-    gtmCode: ["GTM-WJBN353"], // 可放置多個
-    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
+    gtmCode: ["GTM-M3ZG5V54"], // 可放置多個
+    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主1
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -38,7 +38,7 @@
 } */
 .bg{width: 100%;position: fixed;top: 0;left: 0;right: 0;bottom: 0;
   //background-image: url("@/section/s1/bg.jpg");background-size:calc(600 * 100vw / 1920) auto;
-  background:#1C96D4;
+  background:#731620;
   .bg1{
   position: absolute;top: 0;right: 0;width:calc(1500 * 100vw / 1920); 
   height:65%;filter: blur(2px);
