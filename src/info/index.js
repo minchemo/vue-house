96dd8aabac8dc83702a8565072daa3d0258f6aa1
molly <molly8358158@gmail.com>
2023-10-05 09:32:25 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -9,21 +9,21 @@ export default {
     address1: "接待會館",//按鈕區的--- 如空白會只呈現地址
     address2: "接待會館",//map點下確認的--- 如空白會顯示"導航地址"
     address: "台南市新營區金華路二段282號(隔壁)",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3664.2613488514044!2d120.29530227379603!3d23.306274605684493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e85d529c6fa85%3A0x59e69a54dace07b8!2zNzMw5Y-w5Y2X5biC5paw54ef5Y2A6YeR6I-v6Lev5LqM5q61Mjgy6Jmf!5e0!3m2!1szh-TW!2stw!4v1685288845074!5m2!1szh-TW!2stw",
-    googleLink: "https://goo.gl/maps/P8An6DZ5ugQVZDQ6A",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3664.2697685412986!2d120.29751899999998!3d23.305969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDE4JzIxLjUiTiAxMjDCsDE3JzUxLjEiRQ!5e0!3m2!1szh-TW!2stw!4v1696468145644!5m2!1szh-TW!2stw",
+    googleLink: "https://maps.app.goo.gl/sCGe2tn9S5cx57279",
     phone: "06-6026333",
     fbLink: "https://www.facebook.com/100084318365743",
     fbMessage: "https://m.me/100084318365743",
     caseName: caseName,
     houseInfos: [
-      ["投資興建", "XXX"],
-        ["產品規劃", "XXX"],
-     //   ["建築代銷", "上揚國際建築團隊"],
-         /* ["公設設計", ""],
-        ["景觀設計", ""],
-        ["燈光設計", ""],
-        ["基地面積", ""],
-        ["格局規劃", ""],*/
+        ["投資興建", "清景甯建設股份有限公司"],
+        ["營造公司", "威暘營造"],
+        ["建築設計", "曾永信建築師事務所"],
+        ["公設設計", "曾永信建築師事務所"],
+        ["燈光設計", "日光照明設計"],
+        ["基地面積", "514坪"],
+        ["企劃銷售", "上揚國際實業股份有限公司"],
+        ["房型規劃", "2-4房"],
     ],
     gtmCode: ["GTM-PKH9LVK6"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
