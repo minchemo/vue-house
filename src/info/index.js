7ff8ab41500cb642a880f20834947c93b0708bab
xcatx <xcatx796@gmail.com>
2023-10-13 20:43:50 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -25,7 +25,7 @@ export default {
         ["基地面積", ""],
         ["格局規劃", ""],*/
     ],
-    gtmCode: ["GTM-WBVL46WZ"], // 可放置多個
+    gtmCode: ["GTM-WBVL46WZ","GTM-P8K5GNZG"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
