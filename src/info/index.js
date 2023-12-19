0297a9ec0525cf8ba3c82d052b9853c08358167a
molly <molly8358158@gmail.com>
2023-12-19 14:40:47 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -19,7 +19,7 @@ export default {
   ],
 
   gtmCode: ["MXHQL97D"], // 可放置多個
-  recaptcha_site_key_v3: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN",
+  recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN",
   recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
   recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
   order: {
