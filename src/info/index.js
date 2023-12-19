1db707f63432bd14ef60aeea504e6c2683c3fb9c
molly <molly8358158@gmail.com>
2023-12-19 14:26:27 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -18,8 +18,8 @@ export default {
     ["企劃設計", "城豐國際有限公司"],
   ],
 
-  gtmCode: ["WCNSDTS"], // 可放置多個
-  recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
+  gtmCode: ["MXHQL97D"], // 可放置多個
+  recaptcha_site_key_v3: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN",
   recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
   recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
   order: {
