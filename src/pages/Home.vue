fe71a3dfcc45e550f66956970ae0d5800fde0d85
xcatx <xcatx796@gmail.com>
2024-01-15 16:57:30 +0800

    OK

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -227,8 +227,10 @@ img {
 <script setup>
 import info from "@/info"
 import S1 from "@/section/s1.vue"
-import S2 from "@/section/s2.vue"
 // import S2v from "@/section/s2v.vue"
+// import S11 from "@/section/s11.vue"
+/* 
+import S2 from "@/section/s2.vue"
 import S3 from "@/section/s3.vue"
 import S4 from "@/section/s4.vue"
 import S5 from "@/section/s5.vue"
@@ -237,10 +239,7 @@ import S7 from "@/section/s7.vue"
 import S8 from "@/section/s8.vue"
 import S9 from "@/section/s9.vue"
 import S10 from "@/section/s10.vue"
-// import S11 from "@/section/s11.vue"
-/* 
-import S2 from "@/section/s2.vue"
-import S3 from "@/section/s3.vue" */
+ */
 import Order from "@/section/order.vue"
 import Nav from "@/layout/navbar.vue"
 import { onMounted, ref } from "vue"
