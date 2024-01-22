e540ef449435001b1154322fced8064d66e58548
molly <molly8358158@gmail.com>
2024-01-22 13:41:29 +0800

    修改

--- src/section/form/houseInfo.vue
+++ src/section/form/houseInfo.vue
@@ -9,8 +9,8 @@
                     <div class="info-items mt-4 w-full grid grid-cols-1 md:grid-cols-2 ">
                         <div class="item font-bold flex items-center w-full whitespace-nowrap"
                             v-for="item in info.houseInfos">
-                            <p class="mr-5 border-l-2 border-[#fff] pl-2 text-[#fff]" v-html="item[0]"></p>
-                            <p class="whitespace-pre-line leading-normal text-left text-[#fff] font-normal"
+                            <p class="mr-5 border-l-2 border-[#E60012] pl-2 text-[#000]" v-html="item[0]"></p>
+                            <p class="whitespace-pre-line leading-normal text-left text-[#000] font-normal"
                                 v-html="item[1]">
                             </p>
                         </div>
@@ -56,7 +56,7 @@
     .title {
         font-size: size(40);
         font-weight: 700;
-        color: #fff;
+        color: #000;
         margin: 0 auto 1em auto;
     }
 
