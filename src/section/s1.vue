ec623ac2d91a4e3ec09ffb607f176e67bcff6a0e
xcatx <xcatx796@gmail.com>
2024-02-23 13:59:34 +0800

    +alt

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -8,7 +8,7 @@
    <img src="./s1/logo.svg" alt="恆合機構×中山北路" class="logo" alt="" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000" v-if="!$isMobile()" />
    <img src="./s1/logom.svg" alt="恆合機構×中山北路" class="logo" alt="" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000" v-else />
     
-    <div class="t1">
+    <div class="t1" title="恆合建設">
     <img src="./s1/t2.svg" class="t2" alt="中山北路旁．雙綠帶金軸 捷運雙連站頂歸精品建築" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000"/>
 
     <div class="t3" data-aos="fade-up" v-if="$isMobile()">即將公開</div>
