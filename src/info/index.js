25303a7d4569389ceb16d1dd7985fbc44f2694b7
molly <molly8358158@gmail.com>
2024-03-01 11:57:00 +0800

    修改電話

--- src/info/index.js
+++ src/info/index.js
@@ -2,7 +2,7 @@ export default {
     address: "台南市北區北安路一段314號",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.907524556433!2d120.17508249999999!3d22.990427799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e779fe9013f79%3A0xca3d2dcdce33b1d3!2z5pil56aP5b6h6YK4IOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1664269427798!5m2!1szh-TW!2stw",
     googleLink: "https://goo.gl/maps/217W4vMcNfvRetCj8",
-    phone: "06-283-5866​",
+    phone: "06-251-1157​",
     fbLink: "https://www.facebook.com/105225588476358/",
     fbMessage: "https://m.me/105225588476358/",
     caseName: "春福御邸",
