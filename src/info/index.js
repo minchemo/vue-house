b68ab4394eae4c9160f42451757a446b669141c2
molly <molly8358158@gmail.com>
2024-06-20 11:04:38 +0800

    GTM

--- src/info/index.js
+++ src/info/index.js
@@ -20,7 +20,7 @@ export default {
         //["投資興建", "協勝建設股份有限公司"],
         //["企劃銷售", "寰宇動力廣告有限公司"],
     ],
-    gtmCode: ["GTM-A12345678"], // 可放置多個
+    gtmCode: ["GTM-W2Z2B6ND","GTM-KXCTSBDN"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
