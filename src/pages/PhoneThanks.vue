6a23e7d05eaf1a3399ffce19a4b63a74843b0106
xcatx <xcatx796@gmail.com>
2024-06-28 15:11:09 +0800

    改感謝頁

--- src/pages/PhoneThanks.vue
+++ src/pages/PhoneThanks.vue
@@ -1,54 +1,29 @@
 <template>
     <div class="thanks">
-        <a href="/">
-            <img src="//h65.tw/img/thank-img.png" alt="thank" class="img">
-            <img src="//h65.tw/img/thank-img-hover.png" alt="thank" class="img-hover">
-        </a>
+        <img src="@/section/form/thanks.webp" class="logo" alt="logo">
+        <img src="@/section/form/thanksm.webp" class="logom" alt="logo">
     </div>
 </template>
 <style lang="scss" scoped>
 .thanks {
     width: 100vw;
     height: 100vh;
-    background: url('//h65.tw/img/thank_bg.png');
+    background: url("@/section/s1/bg1.jpg");
     background-size: cover;
     background-position: center;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    a {
-        .img {
-            position: absolute;
-            opacity: 1;
-        }
-
-        .img-hover {
-            opacity: 0;
-        }
-
-        &:hover {
-            .img {
-                opacity: 0;
-            }
-
-            .img-hover {
-                opacity: 1;
-            }
-        }
-    }
+  .logo{width:60vw;}
+  .logom{display: none;}
 }
 
 @media screen and (max-width: 767px) {
     .thanks {
-        a {
-
-            .img,
-            .img-hover {
-                width: 90vw;
-            }
-        }
+    background-image: url("@/section/s1/bg1m.jpg");
+  .logo{display: none;}
+  .logom{width:70vw;display: block;}
     }
 }
 </style>
