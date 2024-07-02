8d8d64708b04fce0d51d45cd04379885eb1c25a1
xcatx <xcatx796@gmail.com>
2024-07-02 10:44:49 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
   caseName: caseName,
   houseInfos: [
   ],
-  gtmCode: ["GTM-TNHNNFWL"], // 可放置多個
+  gtmCode: ["GTM-P3JCJ3HC"], // 可放置多個
   recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
   //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
   //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
