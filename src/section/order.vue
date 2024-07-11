4f8f88c846853460eb6bbdf034069622a6672d44
molly8351858 <molly8358158@gmail.com>
2024-07-11 17:24:33 +0800

    修改

--- src/section/order.vue
+++ src/section/order.vue
@@ -25,7 +25,7 @@
               <input type="text" placeholder="手機" class="input w-full rounded-none" :value="formData.phone"
             @input="(event) => (formData.phone = event.target.value)" /></label>
 
-          <label class="row" v-if="info.room_type"><span>需求房型</span>
+          <label class="row" v-if="info.room_type"><span>需求房型<span>(必填)</span></span>
             <select class="select w-full rounded-none bg-white" v-model="formData.room_type">
            <option value="" selected disabled>請選擇房型</option> 
             <option v-for="room in info.room_type" :value="room" v-text="room"></option>
@@ -36,14 +36,14 @@
             <option v-for="budget in info.budget" :value="budget" v-text="budget"></option>
           </select>
         </label>
-          <label class="row"><span>居住縣市</span>
+          <label class="row"><span>居住縣市<span>(必填)</span></span>
           <select class="select w-full rounded-none" v-model="formData.city">
             <option value="" selected disabled>請選擇城市</option>
             <option v-for="city in cityList" :value="city.value">
               {{ city.label }}
             </option>
           </select></label>
-          <label class="row"><span>居住地區</span>
+          <label class="row"><span>居住地區<span>(必填)</span></span>
           <select class="select w-full rounded-none" v-model="formData.area">
             <option value="" selected disabled>請選擇地區</option>
             <option v-for="area in areaList" :value="area.value">
@@ -194,7 +194,7 @@
     align-items:center;
       > span{
         color: #000;
-        width: 5.5em;
+        width: 7.7em;
         text-align: left;padding-left:1em ;
         > span{color: #F00;font-size: 12px;}
       }
@@ -357,7 +357,7 @@ const formData = reactive({
 })
 
 //非必填
-const bypass = ["project", "msg", "people", "email", "room_type","budget", "city", "area"]
+const bypass = ["project", "msg", "people", "email","budget"]
 
 //中文對照
 const formDataRef = ref([
