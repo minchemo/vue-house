dc6e1a0cbd02bc091c1e83bf3819239f186cca93
xcatx <xcatx796@gmail.com>
2024-07-18 14:31:46 +0800

    ok

--- src/info/index.js
+++ src/info/index.js
@@ -11,7 +11,7 @@ export default {
     address: "嘉義市西區竹圍路跟四維路口",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3659.275527864753!2d120.42822220000001!3d23.486583300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDI5JzExLjciTiAxMjDCsDI1JzQxLjYiRQ!5e0!3m2!1szh-TW!2stw!4v1721181681181!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/ozbu3bYW6dskFvvE8",
-    phone: "05-2321-999",
+    // phone: "05-2321-999",
     fbLink: "https://www.facebook.com/61562889772661/",
     fbMessage: "https://m.me/61562889772661/",
     caseName: caseName,
--- src/section/s1.vue
+++ src/section/s1.vue
@@ -5,9 +5,12 @@
    <img src="./s1/mo.jpg" class="t0">
 
    -->
-       <img src="@/section/s1/bg.webp" class="bgh" v-if="!$isMobile()">
-       <img src="@/section/s1/bgm.webp" class="bgh" v-else>
-       <!--img src="@/section/s1/gif2.gif" class="gif"-->
+   <img src="./s1/bg.jpg" class="bgh" v-if="!$isMobile()">
+       <img src="./s1/bgm.jpg" class="bgh" v-else>
+       <img src="@/section/s1/1.gif" class="gif">
+       <img src="./s1/txt.svg" class="txt1" v-if="!$isMobile()" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="0">
+       <img src="./s1/txtm.svg" class="txt1" v-else data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="0">
+       
 <!--div class="logo" data-aos="zoom-in" data-aos-duration="2000" data-aos-delay="0">
   <img src="./s1/logo.svg" alt="logo">
   <div class="earth">
@@ -22,74 +25,28 @@
 
 <style lang="scss" scoped>
 @import '@/assets/style/function.scss';
-
+/*
 @keyframes op {
   50% {
      opacity: 0;
   }
+}*/
+ .bgh{position: absolute;width: 100%;height:100%;top:0;left: 0;object-fit: cover;
 }
- .bgh{position: absolute;width: 100%;height:auto;top:0;left: 0;
-top:calc(50% + #{size(-1080*.5)});
+ .gif{position: absolute;width:100%;height:70%;bottom:0;left: 0;
+mix-blend-mode: overlay;opacity: .8;
 }
- .gif{position: fixed;width:size(1920);height:size(2000);top:0;left: 0;
-//mix-blend-mode: lighten;
-opacity: .2;
-}
-
-
 .s1 {
   @apply relative w-full h-screen;
-  min-height: size(900);
+  min-height: size(1000);
   max-height: size(1080);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items:center;
   font-size:size(36);
-  //background: #E60012;
- // background: url("./s1/bg.webp");
-
-  background-size: cover;
-  .t0{position: absolute;width: 100%;top:0vw;left: 0; pointer-events: none;z-index: 10;opacity: .3;}
-
-.hr{position: absolute;bottom: calc(50% + #{size(-1080*.5)});left: 0;right: 0;margin: auto;opacity: .7;
-    width: size(1410);height: 1px;background: #D9E021;}
-
-  .logo{position: relative;
-    width: size(844);
-    margin-top:7vw;
-    margin-bottom:2vw;
-    img{width: 100%;
-    }
-      .earth{position: absolute;
-        top:2vw;left: 0;right: 0;margin: auto;
-        width:size(100);height:size(100);
-        border-radius: 50%;overflow: hidden;
-       // filter:drop-shadow(0 0 .8vw #FFF) drop-shadow(0 0 .2vw #FFF9);
-       box-shadow: 0 0 .9vw #FFF, 0 0 .2vw #FFF;
-       transform: rotate(10deg);
-       white-space: nowrap;
-    img{position: relative;top:-10%;
-      opacity: 1;transform: translateX(-100%);height: 120%;width: auto;
-      animation:earth 7s linear infinite reverse  ;
-    }
-    &::after{content: "";position: absolute;display: block;top: 0;left: 0;width: 100%;height: 100%;background: radial-gradient(100% 84% at 40% 90%, #000 0%, rgba(0, 0, 0, 0.24) 26.74%, rgba(0, 0, 0, 0.00) 60%, rgba(255, 255, 255, 0.00) 70%, rgba(255, 255, 255, 0.50) 100%);}
-      }
-@keyframes earth {
-  to {
-    transform: translateX(0%);
+  //  .t0{position: absolute;width: 100%;top:0vw;left: 0; pointer-events: none;z-index: 10;opacity: .3;}
+  .txt1{position: absolute;
+    width: size(1310);left:0;right: 0;margin: auto;
+    top: size(184);
+    top:calc(50% + #{size(184 - 1080*.5)});
   }
-}
-    
-  }
-  .t1{
-    width: size(1242);
-    margin-bottom:5.2vw;
-  }
-  .coming{
-    width: size(507);
-  }
-    
 
 }
 
@@ -101,39 +58,17 @@ opacity: .2;
 @media screen and (max-width: 767px) {
 
 
+ .gif{width:100%;height:70%;left: 0;}
 
   .s1 {
-    height:calc(100vh - 63px);
-    min-height: sizem(604);
-    max-height: sizem(750);
+    min-height: sizem(667);
+    max-height: sizem(812);
     font-size:sizem(15);
-
- .bgh{
-top:0;
-}
- .gif{width: sizem(800);height:sizem(800);
-}
-
-    
-  .logo{
-    width: sizem(320);
-    margin-top:4vw;
-    margin-bottom:3.5vw;
-      .earth{
-        top:4vw; width:size(210);height:size(210);
-        box-shadow: 0 0 2vw #FFF, 0 0 1vw #FFF;
-       
-
-      }
+  .txt1{
+    width: sizem(328);
+    top: sizem(77);
+    top:calc(50% + #{sizem(77-667*.5)});
   }
-  .t1{
-    width: sizem(297);
-    margin-bottom:16.5vw;
-  }
-  .coming{
-    width: sizem(224);
-  }
-.hr{bottom: -1px;}
 }
 
   
