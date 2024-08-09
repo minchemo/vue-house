27a4cc21b0ba9bfffebdeaea9cf7371785f2e911
xcatx <xcatx796@gmail.com>
2024-08-09 15:36:44 +0800

    金鑰

--- src/info/index.js
+++ src/info/index.js
@@ -29,7 +29,7 @@ export default {
         ["", "項目皆可增減調整"], 
     ],
     gtmCode: ["GTM-W7ZGRNJ"], // 可放置多個
-    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
+    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
