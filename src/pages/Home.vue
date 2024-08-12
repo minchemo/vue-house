446f1a40c104f19d770568f315694ce423434022
molly8351858 <molly8358158@gmail.com>
2024-08-12 11:01:41 +0800

    公設隱藏

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -20,7 +20,7 @@
     <S2 /></div>
     <S3 />
     <S4 />
-    <S5 />
+  <!--<S5 />-->
     <S6 v-if="false"/>
     <Order />
   </div>
@@ -56,7 +56,7 @@ import S1 from "@/section/s1.vue"
 import S2 from "@/section/s2.vue"
 import S3 from "@/section/s3.vue"
 import S4 from "@/section/s4.vue"
-import S5 from "@/section/s5.vue"
+//import S5 from "@/section/s5.vue"
 import S6 from "@/section/s6.vue"
 import Order from "@/section/order.vue"
 import Nav from "@/layout/navbar.vue"
