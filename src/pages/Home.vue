873982171a5ae06219dbe371e8693643c8c1ce8b
xcatx <xcatx796@gmail.com>
2024-08-15 10:58:41 +0800

    OK

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div ref="gtmNoScript" />
-  <!--loading-->
+  <!--loading
   <div
     v-bind:class="{
       'opacity-0': !isLoading,
@@ -9,13 +8,14 @@
     class="transition-all duration-500flex-col flex items-center justify-center fixed w-screen h-screen top-0 left-0 bg-white z-[10000]"
   >
     <img class="w-32" src="//h65.tw/img/loading_w.gif" alt="loading" srcset="" />
-  </div>
-  <!--loading end-->
-  <Nav v-if="config.showNav" />
+  </div>-->
+  <!--loading end
+  <Nav v-if="config.showNav" /> -->
   <div
-    class="home bg-[#fff] overflow-hidden font-['Noto_Sans_TC'] pb-[64px] md:pb-0"
+    class="home bg-[#F8F8F8] overflow-hidden font-['Noto_Serif_TC']"
   >
     <S1 />
+    <!-- 
     <S2 />
     <S3 />
     <S4 />
@@ -25,48 +25,18 @@
     <S8 />
     <S9 />
     <Order />
+     -->
   </div>
 </template>
 
-<style>
-@import "@/assets/style/custom-aos.css";
-img {
-  display: inline;
-  max-width: unset;
-  height: unset;
-  /* margin: 0 auto; */
-}
-
-.home {
-  background: #fff;
-}
-
-@media screen and (max-width: 768px) {
-}
-</style>
 
 <script setup>
-import info from "@/info"
 import S1 from "@/section/s1.vue"
-import S2 from "@/section/s2.vue"
-import S3 from "@/section/s3.vue"
-import S4 from "@/section/s4.vue"
-import S5 from "@/section/s5.vue"
-import S6 from "@/section/s6.vue"
-import S7 from "@/section/s7.vue"
-import S8 from "@/section/s8.vue"
-import S9 from "@/section/s9.vue"
-import Order from "@/section/order.vue"
-import Nav from "@/layout/navbar.vue"
 import { onMounted, ref } from "vue"
 
 import AOS from 'aos';
 
 const isLoading = ref(true)
-const gtmNoScript = ref('')
-const config = ref({
-  showNav: true
-})
 
 onMounted(() => {
   window.onload = function () {
--- src/section/s1.vue
+++ src/section/s1.vue
@@ -1,45 +1,21 @@
 <template>
   <article class="s1">
+    <!-- 
     <img
       class="t0"
-      src="@/section/s1/mo.jpg" alt="0"
+      src="./s1/pc.png" alt="0"
     />
-    <img
-      class="bg"
-      src="@/section/s1/bg.png" alt="bg" v-if="!$isMobile()"
-    />
-    <img
-      class="bg"
-      src="@/section/s1/bgm.png" alt="bg" v-else
-    />
-    
+     -->
     <img
       class="logo"
-      src="@/section/s1/logo.png" alt="logo"
+      src="@/section/s1/t.svg" alt="logo"
       data-aos="fade-up"
       data-aos-delay="0"
     />
-    <div class="t1" data-aos="fade-up" data-aos-delay="200">集賢大道 ｜ 靜巷好宅    </div>
-    <div class="t2" data-aos="fade-up" data-aos-delay="200">2房23坪｜3房36坪<span>即將完工</span></div>
-    <div class="t3" data-aos="fade-up" data-aos-delay="400">
-      接待會館｜新北市三重區仁安街108號<span v-if="!$isMobile()"> 　 </span><br v-else>
-      禮賓專線｜02-8282-8088
-    </div>
-    <!--
-    <img
-      class="logo"
-      src="@/section/s1/logoan.svg"
-      data-aos="fade-up"
-      data-aos-delay="0"
-    />
-    <div class="t1" data-aos="fade-up" data-aos-delay="200">
-      ｜2房23坪 3房36坪 即將完工｜
-    </div>
-    <div class="t2" data-aos="fade-up" data-aos-delay="400">
-      接待會館｜新北市三重區仁安街108號<br />
-      禮賓專線｜02-8282-8088
-    </div>
-    -->
+    <div class="box"></div>
+    <div class="t1" data-aos="fade-up" data-aos-delay="200">網站維護中...</div>
+    <div class="t2" data-aos="fade-up" data-aos-delay="200"><span>我們正在對伺服器進行升級，<br v-if="$isMobile()">升級期間暫時無法訪問。</span>
+      <span>給您帶來不便，敬請見諒。</span></div>
   </article>
 </template>
 
@@ -47,96 +23,65 @@
 @import "@/assets/style/function.scss";
 
 .s1 {
-  @apply w-full relative;
-  @apply bg-cover bg-center bg-no-repeat;
-  @apply flex flex-col items-center justify-center;
-  color: #000;
-  height:calc(100vh + #{size(180)});
-  // min-height: size(900+180);
-  min-height: size(1260);
-  max-height: size(1260);
-//  background-image: url("@/section/s1/bg.jpg");
-  padding-bottom: size(137);
-  line-height: 1.7;
-.t0{position: absolute;top: 0;left: 0;width: 100%;opacity:0 ;z-index: 10;
-  user-select: none;
-  pointer-events: none;}
-.bg{position: absolute;top: 0;left: 0;width: 100%;z-index:3;
-  user-select: none;
-  pointer-events: none;}
-  .logo {
-    position: relative;z-index:4;
-    width: size(622);margin-top: size(380);
+  @apply w-full relative flex flex-col items-center justify-center;
+  color: #58665A;
+  height:100vh;
+  min-height: size(900);
+  line-height: 1.9;
+  text-align: center;
+  .box {
+    background: url("https://i.imgur.com/SgdLNER.jpeg") repeat;
+    background-size: auto;
+    opacity: 0.8;
+    mix-blend-mode: soft-light;
+    user-select: none;
+    pointer-events: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    top: 0;
+    left: 0;
   }
+  .logo {
+    width: size(315);margin-top: size(60);
+  }  
   .t1 {
-    position: relative;z-index:4;
-    margin-top: size(150);
-    font-size: size(40);
-    font-weight: 600;
-    letter-spacing:0.73em;
-    text-indent: .3em;
+    margin-top: size(45);
+    font-size: size(45);
+    font-weight: 700;
+    letter-spacing:0.07em;
+    text-indent: .4em;
   }
   .t2 {
-    position: relative;z-index:4;
-    margin-top: size(30);
-    font-size: size(40);
-    font-weight: 500;
-    letter-spacing:0.3em;
-    text-indent: -.4em;
-    span{color: #fff;background: #E60012;
-      font-size: 0.875em;
-      font-weight: 400;padding: .1em .3em .16em .4em;border-radius: .4em;
-      letter-spacing:0.1em;text-align: center;margin: 0 0 0 0.5em;}
+    margin-top: size(115);
+    font-size: size(27);
+    font-weight: 600;
+    letter-spacing:0.1em;
+    span{display: block;margin-top:0em;}
   }
-  .t3{
-    position: relative;z-index:4;
-    margin-top: size(150);
-    font-size: size(30);
-    font-weight: 500;
-    letter-spacing: 0.2em;
-  }
-}
-
-/* 螢幕尺寸標準 */
-/* 平板尺寸 */
-@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
 }
 
 @media screen and (max-width: 767px) {
   .s1 {
-    height: calc(100vh - 63px);
     min-height: sizem(604);
-    max-height: sizem(604);
-    padding-bottom: sizem(103);
-   //background-image: url("@/section/s1/bgm.jpg");
 
     .logo {
-      width: sizem(230);
-      margin-top: sizem(170);
+      width: sizem(122);
+      margin-top: sizem(41);
     }
     .t1 {
-      margin-top: sizem(85);
-      font-size: sizem(17);
-    //  letter-spacing: sizem(3);
+      margin-top: sizem(26);
+      font-size: sizem(19);
     }
     .t2 {
-      margin-top: sizem(15);
-      font-size: sizem(17);
-    //  letter-spacing: sizem(3);
-    }
-    .t3 {
-      margin-top: sizem(95);
+      margin-top: sizem(45);
       font-size: sizem(13);
-      line-height: 2.2;
+      span{margin-top:1.5em;}
     }
   }
 }
 </style>
 
 <script setup>
-import { computed, getCurrentInstance, ref, inject, onMounted } from "vue"
-const globals = getCurrentInstance().appContext.config.globalProperties
-
-const smoothScroll = inject("smoothScroll")
-const isMobile = computed(() => globals.$isMobile())
 </script>
