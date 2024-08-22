58d2243f075ea782c02a9dba40690ed5886ff214
xcatx <xcatx796@gmail.com>
2024-08-22 13:01:31 +0800

    GTM

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
         ["建築規劃", "林宗良建築師事務所"],
     ],
 
-    gtmCode: ["WW7G63G"], // 可放置多個
+    gtmCode: [""], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz",
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
