171b7680576831c385ac39b1765ccb867fe889ec
xcatx <xcatx796@gmail.com>
2024-09-02 17:21:43 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -27,7 +27,7 @@ export default {
     ["經紀人字號", "(99)北市經證字第00731號"],
 
   ],
-  gtmCode: ["GTM-TNHNNFWL"], // 可放置多個
+  gtmCode: ["GTM-KRZ397T3"], // 可放置多個
   recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
   //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
   //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
