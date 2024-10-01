7f3867cdb394033c421b8dfaa62ac2352f0006bb
molly8351858 <molly8358158@gmail.com>
2024-10-01 10:13:49 +0800

    修改建案資訊

--- src/info/index.js
+++ src/info/index.js
@@ -8,21 +8,19 @@ export default {
     },
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "高雄市楠梓區芎林一街",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.3929277349284!2d120.32715317587568!3d22.75079482641466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0e3a8d104035%3A0xd900026850cca572!2zODEx6auY6ZuE5biC5qWg5qKT5Y2A6IqO5p6X5LiA6KGXMTk26Jmf!5e0!3m2!1szh-TW!2stw!4v1723798378236!5m2!1szh-TW!2stw",
-    googleLink: "https://maps.app.goo.gl/6THX5eCnbaks9ixf8",
+    address: "高雄市楠梓區芎林一街196號",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.3929277349284!2d120.32715317587568!3d22.75079482641466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0e3a8d104035%3A0xd900026850cca572!2zODEx6auY6ZuE5biC5qWg5qKT5Y2A6IqO5p6X5LiA6KGXMTk26Jmf!5e0!3m2!1szh-TW!2stw!4v1727678362369!5m2!1szh-TW!2stw",
+    googleLink: "https://maps.app.goo.gl/JmQEtaxhqJDSt9Wp6",
     phone: "07-352-2333",
     fbLink: "",
     fbMessage: "",
     caseName: caseName,
     houseInfos: [
-        /*,
-        ["建照號碼", "(111)桃市都建執照字第會德00701-02號"],
-        ["經紀人", "(91)北市經證字第00692號 陳子瑞"], 
-        ["建築設計", "弘憲聯合建築師事務所"],
-        ["使照號碼", "王朝雍建築師事務所"],
-        ["行銷企劃", "自售"],
-        */
+        ,
+        ["投資興建", "禾森事業&永盈建設"],
+        ["廣告代銷", "富裕臻實業有限公司"],
+        ["基地面積", "290.89坪"],
+        
     ],
     gtmCode: ["GTM-K2C7KPDV"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
