b6bf57f76917863ec3bef4aad28567176984e8f1
minchemo <lalab996@hotmail.com.tw>
2024-10-16 01:14:30 +0800

    reservation 調整

--- contact-form.php
+++ contact-form.php
@@ -33,7 +33,7 @@ $room_type    = isset($_POST['room_type']) ? $_POST['room_type'] : '';
 $budget       = isset($_POST['budget']) ? $_POST['budget'] : '';
 $reservation_date       = isset($_POST['reservation_date']) ? $_POST['reservation_date'] : '';
 $reservation_time       = isset($_POST['reservation_time']) ? $_POST['reservation_time'] : '';
-$reservation_count       = isset($_POST['reservation_count']) ? $_POST['reservation_count'] : '';
+$reservation_people_count       = isset($_POST['reservation_people_count']) ? $_POST['reservation_people_count'] : '';
     
 # 好站 hj 客製資料：可聯絡時間
 $time_start        = isset($_POST['time_start']) ? $_POST['time_start'] : '';
@@ -266,7 +266,7 @@ $time_end        = isset($_POST['time_end']) ? $_POST['time_end'] : '';
     $mail->FromName = $case_name." - 官網網站"; //設定寄件者姓名
 
     $mail->Subject = $case_name." - 官網網站"; //設定郵件標題
-    $mail->Body = "網站：https://" . $src . "/<BR>姓名：" . $name . "<BR>電話：" . $phone . "<BR>城市：" . $city . $area . "<BR>需求房型：".$room_type."<BR>購屋預算：".$budget."<BR>預約日期：".$reservation_date."<BR>預約時段：".$reservation_time."<BR>預約人數：".$reservation_count."<BR>留言：".$msg."<BR>備註："."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
+    $mail->Body = "網站：https://" . $src . "/<BR>姓名：" . $name . "<BR>電話：" . $phone . "<BR>城市：" . $city . $area . "<BR>需求房型：".$room_type."<BR>購屋預算：".$budget."<BR>預約日期：".$reservation_date."<BR>預約時段：".$reservation_time."<BR>預約人數：".$reservation_people_count."<BR>留言：".$msg."<BR>備註："."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
     $mail->IsHTML(true); //設定郵件內容為HTML
 
     $tomail_arr = explode(",",$tomail);
@@ -299,7 +299,7 @@ $time_end        = isset($_POST['time_end']) ? $_POST['time_end'] : '';
             $url .= "&reservation_datetime=".$datetime;
             $url .= "&reservation_date=".$reservation_date;
             $url .= "&reservation_time=".$reservation_time;
-            $url .= "&reservation_count=".$reservation_count;
+            $url .= "&reservation_people_count=".$reservation_people_count;
 
             // $ch = curl_init();
             // curl_setopt($ch,CURLOPT_URL,$url);
