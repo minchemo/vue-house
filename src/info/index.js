9f694c3f5e238f656fb8c7adb0dbc50ff7c42add
xcatx <xcatx796@gmail.com>
2024-12-05 16:13:56 +0800

    0

--- src/info/index.js
+++ src/info/index.js
@@ -1,4 +1,4 @@
-const caseName = "微笑寓所"
+const caseName = "鳴日之城"
 
 export default {
     meta: {
@@ -8,23 +8,21 @@ export default {
     },
     address1: "接待會館",//按鈕區的--- 如空白會只呈現地址
     address2: "接待會館",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "241新北市三重區吉祥街61號",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.3413185411673!2d121.48696487635216!3d25.090304936020143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442af2a4135146b%3A0xe3513a6e90931f61!2zMjQx5paw5YyX5biC5LiJ6YeN5Y2A5ZCJ56Wl6KGXNjHomZ8!5e0!3m2!1szh-TW!2stw!4v1709622200235!5m2!1szh-TW!2stw",
-    googleLink: "https://maps.app.goo.gl/Y9hqF78fVecC7fqx8",
-    phone: "02-2855-9933",
-    fbLink: "https://www.facebook.com/61556636163046",
-    fbMessage: "https://m.me/61556636163046",
-    line: "https://lin.ee/jX9Xyhz",
+    address: "桃園市桃園區大仁路50巷17號",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.8171403848105!2d121.31995017592463!3d24.972335340770687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34681ec37700bfc7%3A0xa90ca6f99779e83c!2zMzMw5qGD5ZyS5biC5qGD5ZyS5Y2A5aSn5LuB6LevNTDlt7cxN-iZnw!5e0!3m2!1szh-TW!2stw!4v1732777802405!5m2!1szh-TW!2stw",
+    googleLink: "https://maps.app.goo.gl/HyyRkXFi9PcGVRMs9",
+    phone: "07-722-0088",
+    fbLink: "https://www.facebook.com/61557529449820",
+    fbMessage: "https://m.me/61557529449820",
     caseName: caseName,
     houseInfos: [
-        ["投資興建", "茂群國際投資有限公司"],
-        ["建築設計", "曾聰憲建築師"],
-        ["結構設計", "林慶榮結構技師"],
-        ["工程營造", "笙泰營造"],
-        ["企劃銷售", "欣和地產股份有限公司"],
-        ["使用執照", "110重使字第00336號"],
+      ["投資興建", "新月建設股份有限公司"],
+      ["建築設計", "李承洋建築師事務所"],
+      ["建照號碼", "109鶯建字第00258-01號<bg>109鶯建字第00119-02號"],
+      ["行銷團隊", "曜勝廣告有限公司"],
+      ["經紀人", "陳秀琴 (110)桃市經字第001503號<bg>朱立民  (112)北市經證字第02756號"],
     ],
-    gtmCode: ["GTM-W6PJV9QK"], // 可放置多個
+    gtmCode: ["GTM-T65K69SP"], // 可放置多個
     recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
@@ -35,35 +33,39 @@ export default {
         subTitle: "歡迎預約，將有專人與您聯絡，我們將竭誠為您服務",
     },
     //底下2個 沒項目就會隱藏
-    room_type: ["套房","2房","3房"],
-    budget: ["1000-1500萬","1500-2000萬","2000-2500萬","2500-3000萬"],
-    
+    room_type: ["2房","3房","3+1"],
+    budget: ["1000-1500","1500-2000","2000以上"],
     navList: [{
-        name: "最強增值",
-        target: ".s4",
+        name: "新北門戶",
+        target: ".s1",
         offset: "-60",
         offsetmo: "0",
     },{
-        name: "一橋北市",
+        name: "高速樂活",
+        target: ".s2",
+        offset: "-60",
+        offsetmo: "0",
+    },{
+        name: "美術生活",
         target: ".s3",
         offset: "-60",
         offsetmo: "0",
     },{
-        name: "兩代共融",
-        target: ".s6",
+        name: "雙巨擘聯手",
+        target: ".s4",
         offset: "-60",
         offsetmo: "0",
     },{
-        name: "水岸人生",
-        target: ".s7",
-        offset: "-60",
-        offsetmo: "0",
-    },{
-        name: "房貸瘦身",
+        name: "美學地標",
         target: ".s5",
         offset: "-60",
         offsetmo: "0",
-    },{
+  },{
+        name: "質感建材",
+        target: ".s6",
+        offset: "-60",
+        offsetmo: "0",
+  },{
         name: "立即來電",
         target: ".contact-info",
         offset: "",
