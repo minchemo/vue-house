b104ebda1acd91177befb3978d90a4b66be293c1
xcatx <xcatx796@gmail.com>
2024-12-31 11:26:12 +0800

    FB連結

--- src/info/index.js
+++ src/info/index.js
@@ -12,8 +12,8 @@ export default {
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.236786877387!2d120.54459399999998!3d23.703236799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346ec83dda9a4249%3A0x99a875329f001549!2zNjQw6Zuy5p6X57ij5paX5YWt5biC5Lit5q2j6LevMjE26Jmf!5e0!3m2!1szh-TW!2stw!4v1727419349998!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/UeWxH4orsE4985zc7",
     phone: "05-566-0588",
-    fbLink: "https://www.facebook.com/61556636163046",
-    fbMessage: "https://m.me/61556636163046",
+    fbLink: "https://www.facebook.com/61566167571561",
+    fbMessage: "https://m.me/61566167571561",
     line: "https://lin.ee/jX9Xyhz",
     caseName: caseName,
     houseInfos: [
