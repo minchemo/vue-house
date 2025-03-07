fbc2bbdd57994eadee5286dfa24c38e1e417182d
molly8351858 <molly8358158@gmail.com>
2025-03-07 17:14:31 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -21,7 +21,7 @@ export default {
         ["公設設計", "盼達創意事業"],
         ["基地面積", "約2180坪"],
         ["房型規劃", "2-3房"],
-        ["企劃銷售", "麟甯有限公司、麟岑有限公司"],
+        ["企劃銷售", "湘澂國際有限公司"],
        // ["企劃銷售", `<img class="w-full" src="${new URL(`../section/form/comp.png`, import.meta.url).href}">`],
     ],
     gtmCode: ["GTM-KLF25LBV","GTM-MQRTZPS","GTM-M87MK5M"], // 可放置多個
