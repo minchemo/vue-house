58ac754ae0f7c0fb474ff151fb110346dc1978ed
molly8351858 <molly8358158@gmail.com>
2025-03-21 10:50:54 +0800

    修改

--- src/section/order.vue
+++ src/section/order.vue
@@ -154,7 +154,7 @@
 
     <!-- Map -->
     <Map v-if="info.address" />
-  <div
+  <!--div
     class="footer flex items-center justify-center w-full h-[40px] bg-[#302626]"
   >
     <a href="https://www.lixin.com.tw/" target="_blank"
@@ -170,7 +170,7 @@
       class="text-white text-xs"
       >網頁製作</a
     >
-  </div>
+  </div-->
 
   </div>
 </template>
