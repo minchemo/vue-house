ac86d492489ace9c131b1be6bbc6aff8bbdfcb94
xcatx <xcatx796@gmail.com>
2025-05-09 17:02:04 +0800

    message, formData.msg

--- src/section/order.vue
+++ src/section/order.vue
@@ -438,6 +438,7 @@ const send = () => {
   presend.append("utm_medium", utmMedium);
   presend.append("utm_content", utmContent);
   presend.append("utm_campaign", utmCampaign);
+  presend.append("message", formData.msg)
   presend.append("case_code", info.case_code?info.case_code:info.caseid );
 
   // 如果有必填字段为空，返回
