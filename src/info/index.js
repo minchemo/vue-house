18ef32607fb0036a047462071bdcaae4f6028dc1
xcatx <xcatx796@gmail.com>
2025-05-09 13:56:37 +0800

    caseidcase_code

--- src/info/index.js
+++ src/info/index.js
@@ -5,6 +5,8 @@ export default {
         description: caseName,
         keywords: caseName,
     },
+    caseid: "5496014a-12b3-476d-aa7a-002d9144d44a",
+    // case_code: "csl",
     address1: "籌備處",//按鈕區的--- 如空白會只呈現地址
     address2: "",//map點下確認的--- 如空白會顯示"導航地址"
     address: "桃園市八德區豐德二路8號",
@@ -14,7 +16,6 @@ export default {
     fbLink: "https://www.facebook.com/100063880612874/",
     fbMessage: "https://m.me/100063880612874/",
     caseName: caseName,
-    caseid: "5496014a-12b3-476d-aa7a-002d9144d44a",
     houseInfos: [
         /*
         ["空間設計", "相即空間設計"],
--- src/section/order.vue
+++ src/section/order.vue
@@ -442,6 +442,7 @@ const send = () => {
   presend.append("utm_medium", utmMedium);
   presend.append("utm_content", utmContent);
   presend.append("utm_campaign", utmCampaign);
+  presend.append("case_code", info.case_code?info.case_code:info.caseid );
 
   // 如果有必填字段为空，返回
   if (!pass) {
