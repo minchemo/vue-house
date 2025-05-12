2e4a314a94d71a264f684af5e0475dd7db7fa46e
xcatx <xcatx796@gmail.com>
2025-05-12 13:53:40 +0800

    無留言

--- src/section/order.vue
+++ src/section/order.vue
@@ -432,7 +432,9 @@ const send = () => {
     presend.append(key, value)
   }
 }
-
+if (formData.msg.trim() === "") {
+  formData.msg = "無留言";
+}
   
   presend.append("utm_source", utmSource);
   presend.append("utm_medium", utmMedium);
