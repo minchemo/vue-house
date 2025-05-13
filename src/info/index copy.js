1bf99c2e798b63e186d599c71d48c049680d572c
molly8351858 <molly8358158@gmail.com>
2025-05-13 16:42:31 +0800

    串接新發信平台

--- src/info/index copy.js
+++ src/info/index copy.js
@@ -1,10 +1,13 @@
 const caseName = "微笑雲朵"
 
 export default {
-    caseid: "84e5fe74-4666-4794-b6e2-bc680c2ae4ed",
-    case_code: "cloud",
-    address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
-    address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
+    meta: {
+        title: caseName,
+        description: caseName,
+        keywords: caseName,
+    },
+    address1: "接待會館",//按鈕區的--- 如空白會只呈現地址
+    address2: "接待會館",//map點下確認的--- 如空白會顯示"導航地址"
     address: "雲林縣斗六市中正路216號",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.236786877387!2d120.54459399999998!3d23.703236799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346ec83dda9a4249%3A0x99a875329f001549!2zNjQw6Zuy5p6X57ij5paX5YWt5biC5Lit5q2j6LevMjE26Jmf!5e0!3m2!1szh-TW!2stw!4v1727419349998!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/UeWxH4orsE4985zc7",
@@ -22,42 +25,20 @@ export default {
        /* ["使用執照", "110重使字第00336號"],*/
     ],
     gtmCode: ["GTM-MCP2RFPF"], // 可放置多個
+    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
-    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
     order: {
         title: "預約賞屋",
-        //   subTitle: "歡迎預約，將有專人與您聯絡，我們將竭誠為您服務",
+     //   subTitle: "歡迎預約，將有專人與您聯絡，我們將竭誠為您服務",
     },
-    // 底下2個 沒項目就會隱藏
-    // 控制固定板的新欄位
-    // room_type: ["兩房","三房"],
-    selectFields: {
-        room_type: {
-          title: "需求房型",
-          hold: "請選擇房型",
-          option: ["1房", "1+1房", "2房"],
-          bypass:false,
-        },
-        budget: {
-          title: "購屋預算",
-          hold: "請選擇區間",
-          option: ["700-1000萬"],
-          bypass:false, //必填開啟使用
-        },
-        /*
-        use_type: {
-          title: "使用用途",
-          hold: "請選擇用途",
-          option: ["自住", "投資", "租賃"]
-      },
-        */
-  },
+    //底下2個 沒項目就會隱藏
+    room_type: ["1房","1+1房","2房"],
+    budget: ["700-1000萬"],
 
-    navList: [
-        {
+    navList: [{
         name: "微笑雲朵",
         target: ".s1",
         offset: "0",
@@ -100,5 +81,6 @@ export default {
         offset: "",
         type: 'btn'
     },
-    ],
+],
+
 }
