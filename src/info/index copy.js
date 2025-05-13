93707ec83da7772f413be23d5736d63e3d492bd7
molly8351858 <molly8358158@gmail.com>
2025-05-13 14:39:43 +0800

    串接新發信表單

--- src/info/index copy.js
+++ src/info/index copy.js
@@ -1,8 +1,11 @@
 const caseName = "福泰侘"
 
 export default {
-    caseid: "1d2db7f2-157b-4a33-acbc-f4abfde91846",
-    case_code: "test",
+    meta: {
+        title: caseName,
+        description: caseName,
+        keywords: caseName,
+    },
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
     address: "大園科一街171號（大園國中旁）",
@@ -14,50 +17,27 @@ export default {
     caseName: caseName,
     houseInfos: [
         /*
-        ["空間設計", "相即空間設計"],
-        ["企劃銷售", "低碳健康樂活行銷"],
-        ["建照號碼", "(111)桃市都建執照字第會德00701-02號"],
-        ["經紀人", "(91)北市經證字第00692號 陳子瑞"], 
-        ["建築設計", "弘憲聯合建築師事務所"],
-        ["使照號碼", "王朝雍建築師事務所"],
-        ["行銷企劃", "自售"],
         */
+        ["投資興建", "福泰開發建設有限公司"],
+        ["營造公司", "祥鼎營造"],
+        ["建照號碼", "(113)桃市都建執照字第會觀00331-01號"],
+        ["發照日期", "113年11月01日"],
+        ["行銷企劃", "允竑廣告股份有限公司"],
     ],
     gtmCode: ["GTM-WZMP4MCX"], // 可放置多個
+    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
-    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
     order: {
         title: "預約賞屋",
         subTitle: "若想了解更多資訊，歡迎填寫表單或來電洽詢，將由專人為您服務，謝謝！",
-        subTitle_mo: "若想了解更多資訊，歡迎填寫表單或來電洽詢<br>將由專人為您服務，謝謝！"
+        subTitle_mo: "若想了解更多資訊，歡迎填寫表單或來電洽詢<br>將由專人為您服務，謝謝！",
     },
-    // 底下2個 沒項目就會隱藏
-    // 控制固定板的新欄位
-    // room_type: ["兩房","三房"],
-    selectFields: {
-        room_type: {
-          title: "需求房型",
-          hold: "請選擇房型",
-          option: ["兩房", "三房"],
-          bypass:false,
-        },
-        //budget: {
-        //  title: "購屋預算",
-        //  hold: "請選擇區間",
-        //  option: ["1000", "2000", "3000"],
-        //  bypass:true, //必填開啟使用
-        //},
-        /*
-        use_type: {
-          title: "使用用途",
-          hold: "請選擇用途",
-          option: ["自住", "投資", "租賃"]
-      },
-        */
-  },
+    //底下2個 沒項目就會隱藏
+    room_type: ["兩房","三房"],
+    //budget: ["1000萬以下","1000~1500萬","1500~2000萬","2000~2500萬","2500~3000萬","3000~5000萬","5000萬以上"],
 
     navList: [{
             name: "未來光景",
@@ -113,4 +93,5 @@ export default {
             type: 'btn'
         },
     ],
+    
 }
