c4c82aec8a54296aa5de6d722439b525effcbd55
ruth <ruth@lixin.com.tw>
2025-06-10 16:56:51 +0800

    發信系統更新

--- src/info/index.js
+++ src/info/index.js
@@ -1,102 +1,106 @@
-const caseName = "AI慕光城"
+//const caseName = "AI慕光城"
+const caseName = "000"
 
 export default {
-  meta: {
-    title: caseName,
-    description: caseName,
-    keywords: caseName,
-  },
-  address1: "接待會館", //按鈕區的--- 如空白會只呈現地址
-  address2: "接待會館", //map點下確認的--- 如空白會顯示"導航地址"
-  address: "高雄市前鎮區凱旋四路688號旁",
-  googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d920.8769437338581!2d120.318623286795!3d22.59750600884785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDM1JzUxLjEiTiAxMjDCsDE5JzA4LjYiRQ!5e0!3m2!1szh-TW!2stw!4v1736820168576!5m2!1szh-TW!2stw",
-  googleLink: "https://maps.app.goo.gl/FJiANsmqtxrAN8tK7",
-  phone: "07-8216677",
-  fbLink: "https://www.facebook.com/one.skyline",
-  fbMessage: "https://m.me/one.skyline/",
-  caseName: caseName,
-  houseInfos: [
-    ["投資興建", "清景麟地產有限公司 & 三地建築有限公司"],
-    ["廣告行銷", "嘉揚企業股份有限公司"],
-    ["建築規劃", "新加坡建築巨擘SCDA Architects"],
-    ["建築字號", "(113)高市工建築字第00843號"],
-    ["樓層規劃", "地上24樓地下5層"],
-  ],
-  gtmCode: ["GTM-NK2X6RRC", "GTM-5MQGHTKJ"], // 可放置多個
-  recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
-  //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
-  //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
-  recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
-  recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
-  order: {
-    title: "感謝首波預約貴賓<br>此為第二階段預約",
-    subTitle: "",
-    //subTitle_mo: "若想了解更多資訊，歡迎填寫表單或來電洽詢<br>將由專人為您服務，謝謝！",
-  },
-  // 底下2個 沒項目就會隱藏
-  room_type: ["兩房", "三房", "店面"],
-  //budget: ["自住","投資","置產"],
-
-  navList: [
-    /*
-        {
-            name: "雙核心地段",
-            target: ".s3",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "交通中軸",
-            target: ".s4",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "水岸生活",
-            target: ".s5",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "菁英學區",
-            target: ".s6",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "安心品牌",
-            target: ".s7",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "電梯店墅",
-            target: ".s8",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "嚴選建材",
-            target: ".s9",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "傳家首席",
-            target: ".s10",
-            offset: "-60",
-            offsetmo: "0",
-        },{
-            name: "立即來電",
-            target: ".contact-info",
-            offset: "",
-            type: 'btn'
+    caseid: "987e4a16-db4e-49d4-8906-29dae98bf805",
+    case_code: "one-skyline",
+    address1: "接待會館", //按鈕區的--- 如空白會只呈現地址
+    address2: "接待會館", //map點下確認的--- 如空白會顯示"導航地址"
+    address: "高雄市前鎮區凱旋四路688號旁",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d920.8769437338581!2d120.318623286795!3d22.59750600884785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDM1JzUxLjEiTiAxMjDCsDE5JzA4LjYiRQ!5e0!3m2!1szh-TW!2stw!4v1736820168576!5m2!1szh-TW!2stw",
+    googleLink: "https://maps.app.goo.gl/FJiANsmqtxrAN8tK7",
+    phone: "07-8216677",
+    fbLink: "https://www.facebook.com/one.skyline",
+    fbMessage: "https://m.me/one.skyline/",
+    caseName: caseName,
+    houseInfos: [
+        ["投資興建", "清景麟地產有限公司 & 三地建築有限公司"],
+        ["廣告行銷", "嘉揚企業股份有限公司"],
+        ["建築規劃", "新加坡建築巨擘SCDA Architects"],
+        ["建築字號", "(113)高市工建築字第00843號"],
+        ["樓層規劃", "地上24樓地下5層"],
+    ],
+    gtmCode: ["GTM-NK2X6RRC", "GTM-5MQGHTKJ"], // 可放置多個
+    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
+    //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
+    //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
+    recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
+    recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
+    order: {
+        title: "感謝首波預約貴賓<br>此為第二階段預約",
+        subTitle: "",
+        //subTitle_mo: "若想了解更多資訊，歡迎填寫表單或來電洽詢<br>將由專人為您服務，謝謝！",
+    },
+    // 底下2個 沒項目就會隱藏
+    //budget: ["自住","投資","置產"],
+    selectFields: {
+        room_type: {
+            title: "需求房型",
+            hold: "請選擇房型",
+            option: ["兩房", "三房", "店面"],
+            bypass: false,
         },
-        {
-              name: "地圖導航",
-              target: ".gmap",
-              offset: "",
-              type: "btn",
-        }
-        ,{
-            name: "立即預約",
-            target: ".order",
-            offset: "",
-            type: 'btn'
-        },
-    */
-  ],
+    },
+    navList: [
+        /*
+            {
+                name: "雙核心地段",
+                target: ".s3",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "交通中軸",
+                target: ".s4",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "水岸生活",
+                target: ".s5",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "菁英學區",
+                target: ".s6",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "安心品牌",
+                target: ".s7",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "電梯店墅",
+                target: ".s8",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "嚴選建材",
+                target: ".s9",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "傳家首席",
+                target: ".s10",
+                offset: "-60",
+                offsetmo: "0",
+            },{
+                name: "立即來電",
+                target: ".contact-info",
+                offset: "",
+                type: 'btn'
+            },
+            {
+                  name: "地圖導航",
+                  target: ".gmap",
+                  offset: "",
+                  type: "btn",
+            }
+            ,{
+                name: "立即預約",
+                target: ".order",
+                offset: "",
+                type: 'btn'
+            },
+        */
+    ],
 }
