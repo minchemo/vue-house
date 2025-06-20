e0028cacb28df70d5043e7cb1361b74b4e233c29
molly8351858 <molly8358158@gmail.com>
2025-06-20 14:16:05 +0800

    修改

--- src/section/order.vue
+++ src/section/order.vue
@@ -636,7 +636,7 @@ const send = () => {
       &phone=${formData.phone}
       &email=${formData.email}
       &cityarea=${formData.city}${formData.area}
-      &msg=${formData.room_type}；${formData.msg}
+      &msg=${formData.msg}
       &utm_source=${utmSource}
       &utm_medium=${utmMedium}
       &utm_content=${utmContent}
