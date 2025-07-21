df792699df1fa198cdbb6e2f7102621e81ebe846
xcatx <xcatx796@gmail.com>
2025-07-21 11:19:33 +0800

    更新資訊

--- src/info/index.js
+++ src/info/index.js
@@ -5,12 +5,12 @@ export default {
     case_code: "test",
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "0000",
+    address: "台中市北屯區中平路462號",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.3098394683584!2d121.45248950000001!3d25.0574855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a965069393e1%3A0x3f4f54588cc81ac7!2z5ZyL546L5aSn6YGTS0lORyBQQVJL5o6l5b6F5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1748616767788!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/",
-    phone: "0000",
-    fbLink: "https://www.facebook.com/",
-    fbMessage: "https://m.me/",
+    phone: "04-2293-1199",
+    fbLink: "https://www.facebook.com/dome.smilehouse/",
+    fbMessage: "https://m.me/dome.smilehouse/",
     caseName: caseName,
     houseInfos: [
         ["投資興建", "XX興業股份有限公司"],
@@ -19,7 +19,7 @@ export default {
         ["公設規劃", "XX設計有限公司"],
         ["外觀設計", "XX設計有限公司"],
         ["景觀規劃", "XX園藝工程有限公司"],
-        ["企劃設計", "海沃創意行銷"],
+        ["企劃設計", "欣和地產"],
         /*["投資興建", "長昇事業夥伴 冠奕建設"],
         ["建築設計", "王朝雍建築師事務所"],
         ["工程營造", "又廣營造"],
