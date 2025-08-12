1526565dc8d2e648d98664e6eb3aa2a9e26843f6
xcatx <xcatx796@gmail.com>
2025-08-12 15:39:40 +0800

    改無留言

--- src/section/order.vue
+++ src/section/order.vue
@@ -499,8 +499,10 @@ const send = () => {
 if (formData.gender) {
   formData.name = `${formData.name}(${formData.gender})`;
 }
-
-
+if (formData.msg.trim() === "") {
+    formData.msg = "無留言";
+  }
+  
   // 验证必填字段
   for (const [key, value] of Object.entries(formData)) {
   if (!bypass.includes(key) && (value === "" || value === false)) {
