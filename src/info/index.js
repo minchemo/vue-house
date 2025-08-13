582a10e049a3980ead54a7c3e8a67399b78744af
xcatx <xcatx796@gmail.com>
2025-08-13 10:00:36 +0800

    改連結

--- src/info/index.js
+++ src/info/index.js
@@ -1,7 +1,7 @@
 const caseName = "梓園NEXT"
 export default {
     //caseid: "76ba5df2-d3a5-4d7d-8e41-ee1d8128a3e4",
-    caseid: "1d2db7f2-157b-4a33-acbc-f4abfde91846",
+    caseid: "4af555cf-93db-4f2a-8dc5-555f2169b8b3",
    //case_code: "csl",
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "",//map點下確認的--- 如空白會顯示"導航地址"
@@ -9,8 +9,8 @@ export default {
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.83527141937!2d121.6428191!3d25.0735718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d5338f990cd0f%3A0x5b8bfb03db2385c3!2zMjIx5paw5YyX5biC5rGQ5q2i5Y2A5qif5qi55LqM6LevNDIy6Jmf!5e0!3m2!1szh-TW!2stw!4v1753844618616!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/",
     phone: "02-2690-3888",
-    fbLink: "https://www.facebook.com//",
-    fbMessage: "https://m.me//",
+    fbLink: "https://www.facebook.com/61578977057760/",
+    fbMessage: "https://m.me/61578977057760/",
     caseName: caseName,
     houseInfos: [
         ["投資建設", "XXX"],
