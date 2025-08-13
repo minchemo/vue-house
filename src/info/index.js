dd2234438c22b9072e4e5be93e9b97307773b317
xcatx <xcatx796@gmail.com>
2025-08-13 10:02:19 +0800

    改連結

--- src/info/index.js
+++ src/info/index.js
@@ -5,8 +5,8 @@ export default {
    //case_code: "csl",
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "",//map點下確認的--- 如空白會顯示"導航地址"
-    address: "新北市汐止區樟樹二路422號1樓",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.83527141937!2d121.6428191!3d25.0735718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d5338f990cd0f%3A0x5b8bfb03db2385c3!2zMjIx5paw5YyX5biC5rGQ5q2i5Y2A5qif5qi55LqM6LevNDIy6Jmf!5e0!3m2!1szh-TW!2stw!4v1753844618616!5m2!1szh-TW!2stw",
+    address: "XXXXXX",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3614.1874332013117!2d121.54416325252252!3d25.061635595178924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1szh-TW!2stw!4v1755050513229!5m2!1szh-TW!2stw",
     googleLink: "https://maps.app.goo.gl/",
     phone: "02-2690-3888",
     fbLink: "https://www.facebook.com/61578977057760/",
