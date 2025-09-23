8a84716a0152b338b61c1ec4a34786b705d59b9b
xcatx <xcatx796@gmail.com>
2025-09-23 17:13:10 +0800

    ok

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,33 +9,8 @@
     <img class="w-32" src="//h35.banner.tw/img//loading_w.gif" alt="loading" srcset="">
   </div>
   <!--loading end-->
-   <Nav v-if="config.showNav" />
   <div class="home bg-[#F4F0ED] overflow-hidden font-['Noto_Sans_TC',sans-serif]">
-    
-    <!--   <div class="bg">
-      <img src="@/section/s1/bg1.png" class="bg1" />
-      <img src="@/section/s1/bg2.png" class="bg2" />
-    </div> -->
     <S1 />
-    <!-- <S2v /> -->
-    <S2 />
-    <S3 />
-    <S4 />
-    <S5 />
-    <S6 />
-    <S7 />
-    <S8 />
-    <S9 />
-  <!--
-    <S11 />
-    <div class="bg">
-      <img src="@/section/s1/bg.jpg" />
-      <img src="@/section/s1/bg.jpg" />
-      <img src="@/section/s1/bg.jpg" />
-    </div>
-    <S1new />
-    <S1new2 /> -->
-    <Order />
   </div>
 </template>
 
@@ -215,21 +190,6 @@ img {
 <script setup>
 import info from "@/info"
 import S1 from "@/section/s1.vue"
-import S2 from "@/section/s2.vue"
-// import S2v from "@/section/s2v.vue"
-import S3 from "@/section/s3.vue"
-import S4 from "@/section/s4.vue"
-import S5 from "@/section/s5.vue"
-import S6 from "@/section/s6.vue"
-import S7 from "@/section/s7.vue"
-import S8 from "@/section/s8.vue"
-import S9 from "@/section/s9.vue"
-// import S11 from "@/section/s11.vue"
-/* 
-import S2 from "@/section/s2.vue"
-import S3 from "@/section/s3.vue" */
-import Order from "@/section/order.vue"
-import Nav from "@/layout/navbar.vue"
 import { onMounted, ref } from "vue"
 
 import AOS from 'aos';
