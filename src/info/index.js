dd56251bc992f4172d9bd7c862ee2b9fa29cb2fb
xcatx <xcatx796@gmail.com>
2025-10-20 10:58:43 +0800

    0

--- src/info/index.js
+++ src/info/index.js
@@ -6,7 +6,7 @@ export default {
     address2: "",//map點下確認的--- 如空白會顯示"導航地址"
     address: "台中市后里區三豐路三段1080號",
     googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14544.451486716187!2d120.71196831741628!3d24.30769772085668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34691aa34085f109%3A0x6f2ac81470ffe75a!2zNDIx5Y-w5Lit5biC5ZCO6YeM5Y2A5LiJ6LGQ6Lev5LiJ5q61MTA4MOiZnw!5e0!3m2!1szh-TW!2stw!4v1755848590741!5m2!1szh-TW!2stw",
-    googleLink: "hhttps://maps.app.goo.gl/tNTo6rTh7QiPWuEZ7",
+    googleLink: "https://maps.app.goo.gl/tNTo6rTh7QiPWuEZ7",
     phone: "04-25588028",
     fbLink: "https://www.facebook.com/61579396628225/",
     fbMessage: "https://m.me/61579396628225/",
