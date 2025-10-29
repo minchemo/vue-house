ee98b7fceb13c6f1d32e1778fe67d503110ea111
xcatx <xcatx796@gmail.com>
2025-10-29 10:48:55 +0800

    改給測試主機用的

--- src/info/index.js
+++ src/info/index.js
@@ -1,17 +1,17 @@
-const caseName = "富科大苑"
+const caseName = "測試"
 
 export default {
-    caseid: "a222d6a3-c656-48f1-a58d-df57b56397f0",
+    caseid: "1d2db7f2-157b-4a33-acbc-f4abfde91846",
     //case_code: "fuke",
   address1: "接待中心", //按鈕區的--- 如空白會只呈現地址
   address2: "接待中心", //map點下確認的--- 如空白會顯示"導航地址"
   address: "高雄市楠梓區芎林一街196號",
   googleSrc:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.2275586031965!2d120.32715317603095!3d22.750794826414317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0e3a8d104035%3A0xd900026850cca572!2zODEx6auY6ZuE5biC5qWg5qKT5Y2A6IqO5p6X5LiA6KGXMTk26Jmf!5e0!3m2!1szh-TW!2stw!4v1731266725508!5m2!1szh-TW!2stw",
-  googleLink: "https://maps.app.goo.gl/jGQisT6Uv3CKMHrC7",
-  phone: "07-352-2333",
-  fbLink: "//facebook.com/61563998046848",
-  fbMessage: "//m.me/61563998046848",
+  googleLink: "https://maps.app.goo.gl/",
+  phone: "07-0000",
+  fbLink: "//facebook.com/",
+  fbMessage: "//m.me/",
   caseName: caseName,
   houseInfos: [
     ["投資興建", "禾森事業&永盈建設"],
@@ -19,9 +19,9 @@ export default {
     ["基地面積", "290.89坪"],
 
   ],
-  gtmCode: ["GTM-K2C7KPDV"], // 可放置多個
-  recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
-  //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
+  gtmCode: ["GTM-0000000"], // 可放置多個
+  // recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
+  recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
   //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
   recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
   recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
