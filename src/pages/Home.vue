9d067a811a81bda59f7b54dae5678e82ba9fcf4c
ruth <ruth@lixin.com.tw>
2025-12-19 16:53:56 +0800

    S8 捷運改圖 刪圖層之前

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,6 +16,7 @@
     <S4 />
     <S3 />
     <S7 />
+    <S8 />
     <S9 />
     <S2 />
     <S5 />
@@ -100,6 +101,7 @@ import S1 from "@/section/s1.vue"
 import S2 from "@/section/s2.vue"
 import S3 from "@/section/s3.vue"
 import S7 from "@/section/s7.vue"
+import S8 from "@/section/s8.vue"
 import S5 from "@/section/s5.vue"
 import S4 from "@/section/s4.vue"
 import S6 from "@/section/s6.vue"
