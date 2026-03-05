1fd6e53dbe2ae4f66815bd770245261dc8bee4a8
ruth <ruth@lixin.com.tw>
2026-03-05 09:41:05 +0800

    S2 map

--- src/components/fullview.vue
+++ src/components/fullview.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="viewbox" ref="viewbox">
-        <img ref="viewImg" src="@/section/s2/map.svg" alt="" srcset="">
+        <!-- -->
+        <img ref="viewImg" src="@/section/s2/map.webp" alt="" srcset="">
         <div class="mask" v-bind:class="{ hide: swiped }" v-if="$isMobile()">
             <img src="@/components/fullview/finger.png" alt="" srcset="">
         </div>
@@ -16,10 +17,10 @@
     height: 100%;
     background: #eee;
 
-    > img {
+    >img {
         height: 100%;
         max-width: unset;
-        background:url("@/section/s2/map.webp") 50%;
+        background: url("@/section/s2/map.webp") 50%;
         background-size: 100% auto;
         width: 100%;
     }
@@ -34,9 +35,10 @@
         img {
             height: 100%;
         }
-    > img {
-        width: auto
-    }
+
+        >img {
+            width: auto
+        }
 
 
         .mask {
@@ -72,32 +74,32 @@ import { onMounted, ref, computed, getCurrentInstance } from 'vue';
 const viewbox = ref();
 const viewImg = ref();
 const swiped = ref(false);
-const offsetRatio = 6.5; 
+const offsetRatio = 6.5;
 
 const globals = getCurrentInstance().appContext.config.globalProperties;
 const isMobile = computed(() => globals.$isMobile());
 
 onMounted(() => {
-  viewImg.value.addEventListener('load', () => {
-    if (isMobile.value) {
-      let scroll = new BScroll(viewbox.value, {
-        probeType: 2,
-        scrollX: true,
-        scrollY: true,
-        disableTouch: false,
-        disableMouse: false,
-        bindToWrapper: true,
-        eventPassthrough: "vertical",
-        bounce: false,
-      });
+    viewImg.value.addEventListener('load', () => {
+        if (isMobile.value) {
+            let scroll = new BScroll(viewbox.value, {
+                probeType: 2,
+                scrollX: true,
+                scrollY: true,
+                disableTouch: false,
+                disableMouse: false,
+                bindToWrapper: true,
+                eventPassthrough: "vertical",
+                bounce: false,
+            });
 
-      scroll.scrollTo(scroll.maxScrollX / offsetRatio, 500);
-      setTimeout(() => {
-        scroll.on("scroll", () => {
-          swiped.value = true;
-        });
-      }, 1000);
-    }
-  });
+            scroll.scrollTo(scroll.maxScrollX / offsetRatio, 500);
+            setTimeout(() => {
+                scroll.on("scroll", () => {
+                    swiped.value = true;
+                });
+            }, 1000);
+        }
+    });
 });
 </script>
--- src/section/s2.vue
+++ src/section/s2.vue
@@ -1,11 +1,12 @@
 <template>
   <article class="s2">
-   <!--   -->  <div class="txt">
-    <h3 class="title" data-aos="fade-up" data-aos-delay="0">新北第一環  水岸第一景</h3>
-    <h4 class="subtitle" data-aos="fade-up" data-aos-delay="200">前景×美景  好景成雙</h4>
-    <p class="desc" data-aos="fade-up" data-aos-delay="400">位處重陽重劃區核心地段，奢享山河豪景，靜藏雙公園漫翠，三大建設重心齊發，置身未來增值中央，遙望台北大好江山</p>
-   
-      </div>
+    <!--   -->
+    <div class="txt">
+      <h3 class="title" data-aos="fade-up" data-aos-delay="0">自成一隅繁華，也近一城核心。</h3>
+      <p class="desc" data-aos="fade-up" data-aos-delay="400">
+        「春陽沄集」整排臨路店面,自帶繁華氣息,也貼近內埔核心,擁天后宮、廣濟路、文化路3大商圈,同享國道三號、未來義大健康園區,百億建設座落、人潮車流並進,一墅搶進財位,傳家致富正當時! </p>
+
+    </div>
     <Fullview />
     <div class="caption">基地空拍實景經後製修飾</div>
   </article>
@@ -17,13 +18,22 @@
 .s2 {
   @apply relative overflow-hidden bg-[#00398A] text-[#fff];
   width: 100%;
- // height: size(1059);
-  font-size:size(19);
-  .txt {text-align: center;position: relative;
-  padding-top:3.2em;z-index: 10;margin-bottom:-9.3em ;
+  // height: size(1059);
+  font-size: size(19);
+
+  .txt {
+    text-align: center;
+    position: relative;
+    padding-top: 3.2em;
+    z-index: 10;
+    margin-bottom: -9.3em;
   }
-  .viewbox{z-index: 2;}
+
+  .viewbox {
+    z-index: 2;
+  }
 }
+
 /* 螢幕尺寸標準 */
 /* 平板尺寸 */
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {}
@@ -31,13 +41,19 @@
 @media screen and (max-width: 767px) {
 
   .s2 {
-   // height: sizem(550);
-  font-size:sizem(15);
+    // height: sizem(550);
+    font-size: sizem(15);
 
-  .txt {text-align: justify;
-  padding-top:4.2em;width:sizem(310);margin:auto auto -4.3em auto;
-  }
-  .viewbox{height: sizem(550);}
+    .txt {
+      text-align: justify;
+      padding-top: 4.2em;
+      width: sizem(310);
+      margin: auto auto -4.3em auto;
+    }
+
+    .viewbox {
+      height: sizem(550);
+    }
 
 
   }
