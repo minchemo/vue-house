b3f68b13888376a63f96ee77d2228cc3bd233e3a
ruth <ruth@lixin.com.tw>
2026-03-05 16:09:43 +0800

    S1

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -5,16 +5,24 @@
       <img src="./s1/lineicon.png" class="lineicon" data-aos="zoom-in" data-aos-delay="200" />
     </a>
     <!-- 
-		<img src="./s1/pc.jpg" class="t0">
-		<img src="./s1/mo.jpg" class="t0">  -->
+    <img src="./s1/pc.png" class="t0">
+    <img src="./s1/mo.png" class="t0">-->
     <div class="txt">
       <img src="./s1/bg3.png" data-aos="zoom-in" data-aos-delay="0" data-aos-duration="2000" class="eggbg" alt="蛋黃">
       <img src="./s1/logo.png" alt="logo" data-aos="zoom-in" data-aos-delay="400" data-aos-duration="1000" class="logo">
       <img src="./s1/txt.png" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400" alt="txt" class="txt1">
     </div>
-    <!-- <div class="img"><img src="./s1/b.webp" alt="多會微笑!"><img src="./s1/img.webp" alt="李多慧"><img src="./s1/mark.svg" alt="李多慧"></div> -->
+    <!--  -->
+    <div class="img">
+      <img src="./s1/mark.png" alt="">
+      <img src="./s1/img.png" alt="">
+      <img src="./s1/mark.png" alt="">
+    </div>
+
+
     <img src="./s1/oo.png" alt="oo" class="oo1">
-    <div class="oo2"><img src="./s1/oo.png" alt="oo"></div>
+    <div class="hill"><img src="./s1/hill.png" alt="oo">
+    </div>
 
   </article>
 </template>
@@ -61,36 +69,39 @@
 
   .img {
     position: absolute;
-    top: size(190);
-    left: size(1343);
-    //height: size(992);
-    height: 87%;
+    //top: size(190);
+    //left: size(1343);
+    //height: size(992); 
+    z-index: 2;
 
     img {
       position: relative;
+      top: 23vw;
+      left: 33vw;
 
       &:first-child {
         position: absolute;
-        transform: rotate(-3deg);
-        transform-origin: 90% 150%;
-        animation: an 2s ease-in-out infinite alternate;
-        top: -12.5%;
-        width: 70%;
-        left: -20%;
+        //top: 19.5%;
+        width: 100;
+        //left: -5%;
+        top: 22vw;
+        left: 35vw;
       }
 
       &:last-child {
         position: absolute;
-        top: 29%;
+        top: 680%;
         width: 46%;
-        left: 70%;
+        left: 100%;
       }
 
       &:nth-child(2) {
         height: 100%;
         transform: rotate(1deg);
         transform-origin: 30% 90%;
-        animation: an 2s ease-in-out infinite alternate-reverse;
+        animation: an 3s ease-in-out infinite alternate-reverse;
+        top: 23vw;
+        left: -30vw;
       }
     }
 
@@ -99,19 +110,27 @@
 
   .txt {
     position: absolute;
-    left: 0;
+
+    left: 154px;
     right: 0;
     margin: auto;
-    top: size(357);
-    top: calc(50% + #{size(357 - 1140 * .5)});
+    top: 6.177083vw;
+    top: size(115.2);
+    /* 
+    top: calc(50% + #{size(157 - 1140 * .5)});
+        right: size(-100);
+        top: calc(50% + #{size(970 - 1140 * .5)}); 
+        */
     text-align: center;
     width: size(726);
 
+
+
     .eggbg {
       position: absolute;
-      top: -15%;
+      top: -19%;
       display: block;
-      width: 75%;
+      width: 90%;
       left: -80%;
     }
 
@@ -119,42 +138,60 @@
       position: relative;
       display: block;
       margin: auto;
-      width: 100%;
       z-index: 3;
+      width: 87%;
+
+      top: 4.2vw;
+      left: 14.5vw;
+
     }
 
     .txt1 {
       position: relative;
       display: block;
       margin: 4% auto auto auto;
-      width: 99.2%;
-      left: 1.8%;
       z-index: 3;
+      width: 97%;
+      left: 37%;
+      top: 5.7vw;
     }
+  }
+
+  .txt1 {
+    position: relative;
+    display: block;
+    margin: 4% auto auto auto;
+    z-index: 3;
+    width: 97%;
+    left: 37%;
+    top: 5.7vw;
   }
 
   .oo1 {
     position: absolute;
-    top: size(225);
-    height: size(50);
-    left: size(260);
-    top: calc(50% + #{size(225 - 1140 * .5)});
+    /* 
+    right: size(-100);
+    top: calc(50% + #{size(970 - 1140 * .5)}); 
+    */
     transform: translateX(20%);
-    animation: an 3s ease-in-out infinite alternate;
+    animation: an 9s ease-in-out infinite alternate-reverse;
+    top: 10vw;
+    left: 2vw;
+    width: 120%;
+    top: 30vw;
+
   }
 
-  .oo2 {
+  .hill {
     position: absolute;
-    top: size(970);
     height: size(50);
-    right: size(-100);
-    top: calc(50% + #{size(970 - 1140 * .5)});
-    transform: translateX(-15%);
-    animation: an 2s ease-in-out infinite alternate-reverse;
+    left: 0px;
+    bottom: 200px;
+    /* right: size(-100);
+    top: calc(50% + #{size(970 - 1140 * .5)}); */
 
     img {
-      height: 100%;
-      transform: scaleX(-1);
+      width: 100%;
     }
   }
 }
@@ -220,7 +257,7 @@
       top: calc(50% + #{sizem(140 - 604 * .5)});
     }
 
-    .oo2 {
+    .hill {
       top: sizem(490);
       height: sizem(20);
       right: sizem(-65);
