b8b228c90a5fbf72903942f748e87d9fe8258dca
xcatx <xcatx796@gmail.com>
2026-03-20 11:09:07 +0800

    0

--- src/info/index.js
+++ src/info/index.js
@@ -3,8 +3,8 @@ const caseName = "理享城"
 export default {
     caseid: "1ee49fb7-53f3-47af-80bd-de96cfe7b21e",
     address: "新北市五股區成泰路三段540號",
-    googleSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.964620587833!2d121.45022527608016!3d25.103058935506034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442af50e00ff67b%3A0x73cacf601b48138c!2zMjQ45paw5YyX5biC5LqU6IKh5Y2A5oiQ5rOw6Lev5LiJ5q61NTQw6Jmf!5e0!3m2!1szh-TW!2stw!4v1773043733868!5m2!1szh-TW!2stw",
-    googleLink: "https://maps.app.goo.gl/3FeC2x4S4RKqgmdk9",
+    googleSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d903.2422824596586!2d121.452779!3d25.1029063!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442af00704250e9%3A0x5bea59507228025d!2z55CG5Lqr5Z-O5o6l5b6F5pyD6aSo!5e0!3m2!1szh-TW!2stw!4v1773798492469!5m2!1szh-TW!2stw",
+    googleLink: "https://maps.app.goo.gl/Etxn3zVPiN5VzFVz9",
     phone: "02-2295-3888",
     fbLink: "https://www.facebook.com/wl2.debang/",
     fbMessage: "https://m.me/wl2.debang/",
