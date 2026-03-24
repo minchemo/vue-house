7cc4657ff0cc1f4da394237afafb4fb0a498cd67
ruth <ruth@lixin.com.tw>
2026-03-24 12:02:11 +0800

    GTM  + APP SIZE

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ let script = "<?php include('head.php'); ?>"
 
 console.log('\x1b[33m%s\x1b[0m', '圖片壓縮完成 (vite.config.js 可調整壓縮設定)');
 
-fs.readFile(__dirname + '/dist/index.html', 'utf8', function (err, html) {
+fs.readFile(__dirname + '/dist/index.html','utf8', function (err, html) {
 
     let result = minify(html, {
         removeAttributeQuotes: true,
@@ -14,7 +14,7 @@ fs.readFile(__dirname + '/dist/index.html', 'utf8', function (err, html) {
 
     result = result.replace("</head>", `${script}</head>`)
     fs.writeFileSync(__dirname + '/dist/index.html', result);
-    console.log('\x1b[33m%s\x1b[0m', 'head.php 已寫入 index.html');
+    console.log('\x1b[33m%s\x1b[0m','head.php 已寫入 index.html');
     console.log('');
-    console.log('\x1b[36m%s\x1b[0m', '編譯完成');
+    console.log('\x1b[36m%s\x1b[0m','編譯完成');
 })
--- src/info/index.js
+++ src/info/index.js
@@ -24,7 +24,7 @@ export default {
            ["經 紀 人", "陳怡靜 (104)新北經字第000884號"],
            ["銷售中心", "接待會館：新竹市東區埔頂路125號對面"],*/
     ],
-    gtmCode: ["GTM-5RH473P9",], // 可放置多個
+    gtmCode: ["GTM-5RH473P9", GTM-PLC5C8ZR,], // 可放置多個
 
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
