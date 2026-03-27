37ef445aba1ea88bd03b26cd26f6d3ad6dc43c30
xcatx <xcatx796@gmail.com>
2026-03-27 19:19:47 +0800

    0

--- src/info/index.js
+++ src/info/index.js
@@ -1,6 +1,8 @@
-const caseName = "天好運3"
+const caseName = "0000"
 export default {
-    caseid: "592c1522-ab1f-4c54-99f2-f02c4ae1a428",
+    //caseid: "592c1522-ab1f-4c54-99f2-f02c4ae1a428",
+    
+    caseid: "1d2db7f2-157b-4a33-acbc-f4abfde91846",
     address1: "接待中心",//按鈕區的--- 如空白會只呈現地址
     address2: "接待中心",//map點下確認的--- 如空白會顯示"導航地址"
     address: "新北市土城區科技路27號",
@@ -26,8 +28,8 @@ export default {
         */
     ],
     gtmCode: ["GTM-PHJZS3QV"], // 可放置多個
-    recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
-    //recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
+    //recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
+    recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
     //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
