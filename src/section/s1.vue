d91c2138793c0b653ebb891022242006362e0037
xcatx <xcatx796@gmail.com>
2026-07-02 15:11:09 +0800

    ok

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -1,33 +1,157 @@
 <template>
   <article class="s1 relative" id="s1">
-   <picture>
-  <source
-    media="(max-width: 768px)"
-    srcset="./s1/s1m.jpg"
-  />
-  <img
-    src="./s1/s1.jpg"
-    alt="000"
-  />
-</picture>
+    <div class="cloud">
+      <img src="./s1/cloud.png" alt="cloud" />
+    </div>
+    <picture>
+      <source media="(max-width: 768px)" srcset="./s1/t1m.svg" />
+      <img src="./s1/t1.svg" alt="台北世界級 以我為中心" class="t1" />
+    </picture>
+    <div class="pic">
+      <img src="./s1/img.webp" alt="img" />
+    </div>
+    <picture>
+      <source media="(max-width: 768px)" srcset="./s1/t2m.svg" />
+      <img src="./s1/t2.svg" alt="頂溪帝標．再寫新章2920-5858" class="t2" />
+    </picture>
+    <div class="wave" />
+
 
   </article>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/style/function.scss';
-  .t0{position: absolute;width: 100%;top:0px;left: 0; pointer-events: none;z-index: 9;opacity: .3;}
+
+.t0 {
+  position: absolute;
+  width: 100%;
+  top: 0px;
+  left: 0;
+  pointer-events: none;
+  z-index: 9;
+  opacity: .3;
+}
+
 @keyframes an {
   to {
     transform: translateX(0%);
   }
 }
-  .s1 {
-img{width: 100%;height: 100%;object-fit:cover;}
+
+.s1 {
+  height: min(sizem(750), 100vh - sizem(63)); //最大1200px
+  min-height: sizem(604);
+  background: linear-gradient(180deg, #8080B6 14.96%, #7E7DB4 40.33%, #7876AF 51.42%, #6E6AA6 59.35%, #53488F 70.44%, #7175B0 75.99%, #7E88BF 83.91%, #808BC1 94.22%);
+
+  @media screen and (min-width: 767px) {
+
+  height: min(size(1080), 100vh); //最大1200px
+  min-height: size(900);
+  }
+
+  .img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: .3;
+  }
+
+  .cloud {
+    position: absolute;
+    top: sizem(98);
+    top: calc(50% + #{sizem(98 - 750 * .5)});
+    right: sizem(10);
+    width: sizem(125);
+
+    img {
+      width: 100%;
+      transform: translateX(-30%);
+      animation: an 6s linear alternate infinite;
+    }
+
+    @media screen and (min-width: 767px) {
+      top: size(450);
+    top: calc(70% + #{size(450 - 1080 * .7)});
+      right: size(350);
+      width: size(310);
+    }
+  }
+
+  .pic {
+    position: absolute;
+    top: sizem(400);
+    top: calc(70% + #{sizem(400 - 750 * .7)});
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 100%;
+
+    img {
+      width: 100%;
+    }
+
+    @media screen and (min-width: 767px) {
+      top: size(355);
+    top: calc(70% + #{size(355 - 1080 * .7)});
+      width: size(1150);
+    }
+  }
+
+  .t1 {
+    position: absolute;
+    top: sizem(153);
+    top: calc(55% + #{sizem(153 - 750 * .55)});
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: sizem(17);
+
+    @media screen and (min-width: 767px) {
+      top: size(290);
+    top: calc(70% + #{size(290 - 1080 * .7)});
+      right: size(20);
+      width: size(1250);
+    }
+  }
+
+  .t2 {
+    position: absolute;
+    top: sizem(580);
+    top: calc(70% + #{sizem(580 - 750 * .7)});
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: sizem(185);
+
+    @media screen and (min-width: 767px) {
+      top: size(813);
+    top: calc(70% + #{size(813 - 1080 * .7)});
+      right: size(0);
+      width: size(680);
+    }
+  }
+
+  .wave {
+    position: absolute;
+    left: 0;
+    bottom: 15%;
+    width: 100%;
+    opacity: 0.2;
+    background: url(./s1/wave2.gif) lightgray 0px 0px / 100% 100% no-repeat;
+    mix-blend-mode: overlay;
+    height: 15%;
+
+    @media screen and (min-width: 767px) {
+      height: 32%;
+    bottom: 0;
+    }
+
+  }
 }
 </style>
 <script setup>
-import { computed, getCurrentInstance, ref ,inject} from 'vue';
+import { computed, getCurrentInstance, ref, inject } from 'vue';
 const globals = getCurrentInstance().appContext.config.globalProperties;
 
 const isMobile = computed(() => globals.$isMobile());
--- src/section/s2.vue
+++ src/section/s2.vue
@@ -1,44 +1,99 @@
 <template>
   <article class="s2">
-    <!--    <div class="txt">
-    <img src="./s1/s2.jpg" class="t0" alt="">
-    <img src="./s1/s2m.jpg" class="t0" alt="">
-    <h3 class="title" data-aos="fade-up" data-aos-delay="0">中央軸心盛大開場</h3>
-    <h4 class="subtitle" data-aos="fade-up" data-aos-delay="200">重量級建設共演 台中未來從此綻放</h4>   
-      </div> -->
-
+    <div class="cloud">
+      <img src="./s1/cloud.png" alt="cloud" />
+    </div>
+    <img src="./s1/slogo.svg" class="slogo" alt="">
+    <!--    --> <div class="txt">
+    <picture>
+      <source media="(max-width: 768px)" srcset="./s1/s3t1m.svg" />
+      <img src="./s1/s3t1.svg" alt="頂溪帝標．再寫新章2920-5858" class="t1" />
+    </picture>
+    <p class="d" >偉大的領航者，都用時間證明視野——超過30個重劃區、上萬戶家庭落址<br>
+茂德建設，成就的不只是數字，更是信任——25年，是我們開始定義「世界級」的起點<br><br>
+三橋二快雙捷運，速度格局同步升級；成熟捷運商圈核心、千億建設挹注領漲<br>
+準備看見，大台北從來沒有的眼界</p>   
+      </div>
     <Fullview />
     <picture>
       <source media="(max-width: 768px)" srcset="./s1/s2m.png" />
       <img src="./s1/s2.png" alt="000" class="mm" />
-      <div class="caption">鳥瞰實景拍攝輔以電腦合成示意圖</div>
     </picture>
+      <div class="caption">鳥瞰實景拍攝輔以電腦合成示意圖</div>
   </article>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/style/function.scss';
-
-.t0 {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 5;
-  opacity: .5;
-  width: 100%;
-  pointer-events: none;
+@keyframes an {
+  to {
+    transform: translateX(0%);
+  }
 }
 
 .s2 {
   @apply relative bg-[#818dc1] text-[#fff];
   width: 100%;
   // height: size(1059);
-  font-size: size(15);
+  font-size: size(16);
 
   @media screen and (max-width: 767px) {
     padding: sizem(0) 0 sizem(60) 0;
     height: sizem(740);
     font-size: sizem(12);
+  }
+  .cloud {
+    position: absolute;
+    top: sizem(98);
+    top: calc(50% + #{sizem(58 - 750 * .5)});
+    right: sizem(-20);
+    width: sizem(130);
+    z-index: 12;
+
+    img {
+      width: 100%;
+      transform: translateX(-30%);
+      animation: an 6s linear alternate infinite;
+    }
+
+    @media screen and (min-width: 767px) {
+      top: size(450);
+    top: calc(70% + #{size(217 - 1080 * .7)});
+      right: size(270);
+      width: size(300);
+    }
+  }
+  .slogo{
+    position: absolute;
+    width: sizem(50);
+    top:sizem(15);
+    left: sizem(70);
+    z-index: 12;
+    @media screen and (min-width: 767px) {
+    width: size(125);
+    top:size(95);
+    left:size(173);
+
+    }
+
+  }
+  .txt{
+    position: absolute;
+    width: sizem(310);
+    top:sizem(30);
+    left: 0;right: 0;margin: auto;
+    z-index: 12;
+    text-align: center;
+    @media screen and (min-width: 767px) {
+    width: size(650);
+    top:size(10);
+    }
+    .t1{width: sizem(207);margin-bottom:sizem(-88) ;
+    @media screen and (min-width: 767px) {
+      width: size(615);margin-bottom:size(-88) ;
+    }}
+
+
   }
 
   .mm {
