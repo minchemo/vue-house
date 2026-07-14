7a789b6fb7c65b1c3663672f7a4f7eaa766e8999
xcatx <xcatx796@gmail.com>
2026-07-14 19:01:51 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -25,7 +25,7 @@ export default {
     /*
     */
   ],
-  // gtmCode: ["GTM-MHSDGRVJ"], // 可放置多個
+   gtmCode: ["GTM-MP7M2ZPK"], // 可放置多個
   recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
   //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
   //recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
